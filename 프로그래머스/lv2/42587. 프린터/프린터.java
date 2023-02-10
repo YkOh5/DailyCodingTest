386f90eb786b4a3440feccdbc5d85ecec976c2dd
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-02-10 11:16:00 +0900

    Update 프린터.java

--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -1,33 +1,41 @@
+// 1. 중요도가 존재하지만 기본적으로는 선입선출의 구조이므로 Queue를 이용
+// 2. 모든 인쇄작업의 중요도를 Queue에 추가하고, 하나씩 뽑아내서(대기 위치 - 1) 최고 중요도와 비교
+// 3. 해당 작업의 중요도가 가장 높으면 인쇄 횟수 + 1
+// 4. 가장 높지 않으면 Queue에 다시 추가, 이 때 대기 위치 = 0 이었다면 대기열 끝으로 갱신 (location = Queue.size())
+// 5. 대기 위치 < 0 이면, 인쇄 횟수를 반환
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> print = new LinkedList<>();
         for (int item : priorities) {
-            queue.add(item);
-        }
+            print.add(item);
+        }        
         
-        int priority = 0;
         int cnt = 0;
         
-        while (!queue.isEmpty()) {
-            priority = queue.poll();
-            if (priority >= queue.stream().max(Integer::compare).orElse(-1)) {
+        while (!print.isEmpty()) {
+            int priority = print.poll();
+            // 현재 작업의 중요도가 가장 높은 경우 인쇄 횟수 + 1
+            if (priority >= print.stream().max(Integer::compare).orElse(-1)) {
                 cnt++;
+            // 가장 높지 않은 경우 대기열 끝에 다시 추가
             } else {
-                queue.add(priority);
+                print.add(priority);
+                // 해당 작업물이 인쇄를 요청한 문서인 경우 대기 위치를 대기열 끝으로 갱신
                 if (location == 0) {
-                    location = queue.size();
+                    location = print.size();
                 }
             }
             
+            // 다른 작업물이 인쇄가 되든 대기열 끝으로 밀리든 요청한 문서의 대기 위치는 앞으로 이동
             location--;
-            
+            // 요청한 문서가 인쇄된 경우
             if (location < 0) {
                 break;
             }
-        }
-        
+        }        
         return cnt;
     }
 }
