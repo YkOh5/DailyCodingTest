42eb7ab9e5e06a190b37137c2f27a9742586bac0
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-02-19 10:31:55 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.20 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,23 +1,28 @@
-import java.util.Stack;
+import java.util.*;
+
 class Solution {
     public String solution(String number, int k) {
-        char[] result = new char[number.length() - k];
         Stack<Character> stack = new Stack<>();
-
-        for (int i=0; i<number.length(); i++) {
-            char c = number.charAt(i);
-            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < number.length(); i++) {
+            while (!stack.isEmpty() && cnt < k && stack.peek() - '0' < number.charAt(i) - '0') {
                 stack.pop();
+                cnt++;
             }
-            stack.push(c);
+            
+            if (stack.size() < number.length() - k) {
+                stack.push(number.charAt(i));
+            }
         }
-        for (int i=0; i<result.length; i++) {
-            result[i] = stack.get(i);
+        
+        StringBuilder answer = new StringBuilder();
+        
+        while (!stack.isEmpty()) {
+            answer.insert(0, stack.pop());
         }
-        return new String(result);
+        
+        return answer.toString();
     }
 }
-
-
-
-
