012386ce7c6f93c5b57d3ac271200d9db1084e0d
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-02-22 01:08:28 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,33 @@
+// 1. goal의 모든 원소들에 대해 각 원소와 두 카드 뭉치의 단어들을 순차적으로 비교
+// ex) goal의 첫 번째 단어를 1번 카드 뭉치의 1번 단어와 2번 카드 뭉치의 1번 단어에 비교했을 때, 전자와 일치한다면
+// goal의 두 번째 단어를 1번 카드 뭉치의 2번 단어와 2번 카드 뭉치의 1번 단어에 비교
+// 2. 순서에 맞게 일치하는 단어가 없거나 사용되지 않고 남은 카드가 있다면 "No"를 반환, 그 외 "Yes"를 반환
+
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        for (int i = 0; i < goal.length; i++) {
+            // goal의 단어가 첫 번째 카드 뭉치의 현재 단어와 일치할 때
+            if (goal[i].equals(cards1[idx1])) {
+                cards1[idx1] = "USED";   // 재사용 방지
+                if (idx1 < cards1.length - 1) {
+                    idx1++;   // 비교대상을 카드 뭉치의 다음 단어로
+                }
+            // goal의 단어가 두 번째 카드 뭉치의 현재 단어와 일치할 때
+            } else if (goal[i].equals(cards2[idx2])) {
+                cards2[idx2] = "USED";   // 재사용 방지
+                if (idx2 < cards2.length - 1) {
+                    idx2++;   // 비교대상을 카드 뭉치의 다음 단어로
+                }
+            } else {
+                return "No";
+            }
+        }
+        
+        
+        return "Yes";
+    }
+}
