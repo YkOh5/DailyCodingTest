5c9026f5897b1951234157ec4e04e5fca8588e62
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-03-07 06:13:08 +0900

    Delete 프로그래머스/lv1/12934. 정수 제곱근 판별 directory

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++
@@ -1,8 +0,0 @@
-// 1. 양의 정수 n의 제곱근 x를 1로 나눈 나머지가 0이면, x+1의 제곱을 반환
-// 2. x를 1로 나눈 나머지가 0이 아니면, -1을 반환
-
-class Solution {
-    public long solution(long n) {
-        return (Math.sqrt(n) % 1 == 0)? (long)Math.pow(Math.sqrt(n) + 1, 2) : -1;
-    }
-}
