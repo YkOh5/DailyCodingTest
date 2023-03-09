da6c101f12dd64bb7012327fdfd58c10b00aaceb
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-03-09 11:33:22 +0900

    Delete 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기 directory

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Solution {
-    public long solution(long n) {
-        String[] strArr = Long.toString(n).split("");
-        Arrays.sort(strArr, Collections.reverseOrder());
-        return Long.parseLong(String.join("", strArr));
-    }
-}
