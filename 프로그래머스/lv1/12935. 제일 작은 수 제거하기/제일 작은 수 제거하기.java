5588062b8cc2c1d492226879bc97cb40850d0f3a
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-03-13 10:30:21 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.83 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,30 @@
+// 1. 반복문으로 주어진 int 배열의 원소들 중 최소값의 인덱스를 찾으면서, 각 원소들을 ArrayList에 추가
+// 2. ArrayList에서 최소값의 인덱스에 해당하는 원소를 제거
+// 3. ArrayList의 크기가 0이면 원소가 -1 하나뿐인 int 배열을 반환, 그 외 경우는 ArrayList를 배열로 변환하여 반환
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        int minIdx = 0;
+        
+        // 주어진 배열의 모든 원소들에 대해
+        for (int i = 0; i < arr.length; i++) {
+            // 해당 원소가 기존의 최소값보다 작으면
+            if (arr[i] < min) {
+                min = arr[i];   // 최소값 갱신
+                minIdx = i;   // 최소값의 인덱스 갱신
+            }
+            // 해당 원소를 ArrayList에 추가
+            list.add(arr[i]);            
+        }
+        // ArrayList에서 최소값의 인덱스에 위치한 원소를 제거
+        list.remove(minIdx);
+        
+        // ArrayList의 크기가 0이면 원소가 -1 하나뿐인 int 배열을 반환
+        // 그렇지 않으면 ArrayList<Integer>를 언박싱한 후 배열로 변환하여 반환
+        return list.size() == 0? new int[]{-1} : list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
