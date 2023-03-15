ac29e09727f96eb10f8ebdabcb72e271ff86cb8d
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-03-15 13:23:47 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.77 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        ArrayList<Integer> list = new ArrayList<>();
+        // 문자열 s를 공백을 구분자로 분할하고
+        StringTokenizer st = new StringTokenizer(s, " ");
+        // 정수형 래퍼 클래스로 변환한 후 ArrayList에 추가
+        while (st.hasMoreTokens()) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        // 오름차순으로 정렬
+        Collections.sort(list);
+        // 정렬된 ArrayList의 첫 번째 원소(최소값)와 마지막 원소(최대값)을 이용해서 반환
+        return String.valueOf(list.get(0) + " " + list.get(list.size()-1));
+        
+        
+        // 기초적인 방법으로 최소/최대값을 구하고, StringBuilder를 이용해서 이어붙이는 방식이 훨씬 빠르다
+        // String[] strArr = s.split(" ");
+        // int min = Integer.MAX_VALUE;
+        // int max = Integer.MIN_VALUE;        
+        // for (String item : strArr) {
+        //     if (Integer.parseInt(item) < min) {
+        //         min = Integer.parseInt(item);
+        //     }
+        //     if (Integer.parseInt(item) > max) {
+        //         max = Integer.parseInt(item);
+        //     }
+        // }        
+        // StringBuffer sb = new StringBuffer();
+        // sb.append(min);
+        // sb.append(" ");
+        // sb.append(max);        
+        // return sb.toString();
+        
+        
+        // Stream을 활용하면 처리속도는 다소 떨어지지만, 훨씬 간결한 코드로 해결 가능
+        // String[] strArr = s.split(" ");        
+        // StringBuilder sb = new StringBuilder();
+        // sb.append(Arrays.stream(strArr).mapToInt(Integer::parseInt).min().getAsInt());
+        // sb.append(" ");
+        // sb.append(Arrays.stream(strArr).mapToInt(Integer::parseInt).max().getAsInt());        
+        // return sb.toString();
+    }
+}
