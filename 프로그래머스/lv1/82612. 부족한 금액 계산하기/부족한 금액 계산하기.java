2e1ec7742f6e8cb7322169048bd546df31318924
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-03-21 13:16:41 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.08 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,12 @@
+// 놀이기구를 N번 탑승한 후 차액 : 소지금 - (놀이기구 이용료 * 이용 횟수의 총합)
+// 제한사항에 따라 차액의 크기가 150억을 초과할 수 있으므로, 범위가 -21억~21억 정도인 int형으로는 감당 불가 -> long형 사용
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        // 놀이기구 이용 횟수의 총합
+        long cntSum = count * (1 + count) / 2;
+        
+        // 금액이 부족하지 않으면 0을 반환, 그렇지 않으면 차액의 절대값을 반환
+        return money - price * cntSum >= 0 ? 0 : Math.abs(money - price * cntSum);
+    }
+}
