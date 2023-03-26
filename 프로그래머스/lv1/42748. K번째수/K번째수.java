f76a1bb46207723831f9c2202b6678f91699a5d9
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-03-26 17:16:30 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];     
+        for (int n = 0; n < commands.length; n++) {
+            // 배열 array의 i~j번째 숫자들을 잘라낸 배열
+            int[] subArray = Arrays.copyOfRange(array, commands[n][0]-1, commands[n][1]);
+            // 잘라낸 배열을 오름차순으로 정렬
+            Arrays.sort(subArray);
+            // 정렬된 배열의 k번째 수
+            answer[n] = subArray[commands[n][2]-1];
+        }
+        
+        return answer;
+    }
+}
