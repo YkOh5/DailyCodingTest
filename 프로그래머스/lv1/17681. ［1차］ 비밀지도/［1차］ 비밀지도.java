19738677deee7b2993ecf20d1757d1ab5d774391
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-03-30 19:03:50 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.06 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,16 @@
+// 두 개의 지도 중 어느 하나라도 벽인 부분은 겹쳐진 전체 지도에서도 벽이므로, 암호화된 두 배열에 대해 OR 비트 연산자를 이용
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            // 암호화된 각 정수 배열에서 겹치는 부분끼리의 OR 비트 연산 결과를 2진수 문자열로 변환
+            String row = Integer.toBinaryString(arr1[i] | arr2[i]);
+            // 행을 나타내는 2진수 문자열을 지도의 길이에 맞게 배치하고 2진수 중 1은 벽(#)으로, 0은 공백( )으로 복구
+            answer[i] = String.format("%" + n + "s", row).replace("1", "#").replace("0", " ");
+        }
+        
+        return answer;
+    }
+}
