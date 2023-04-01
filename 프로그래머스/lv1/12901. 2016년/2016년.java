1fe412e86556d911e881d1bf21876ecdca959cb9
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-01 17:03:31 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -3,16 +3,31 @@ import java.time.DayOfWeek;
 
 class Solution {
     public String solution(int a, int b) {
-        String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] dateM = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
-        LocalDate date = LocalDate.of(2016, a, b);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        int dayOfWeekNumber = dayOfWeek.getValue();
+        int dateY = b - 1;
         
-        return day[dayOfWeekNumber - 1];
+        if (a >= 2) {
+            for (int i = 0; i < a - 1; i++) {
+            dateY += dateM[i];
+            }
+        }
+        
+        return day[dateY % 7];
+        
+        
+        
+//         String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        
+//         LocalDate date = LocalDate.of(2016, a, b);
+//         DayOfWeek dayOfWeek = date.getDayOfWeek();
+//         int dayOfWeekNumber = dayOfWeek.getValue();
+        
+//         return day[dayOfWeekNumber - 1];
         
         
         // 단 한 줄의 코드로 해결 가능
-        // return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
+        // return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);        
     }
 }
