c9b2e8555c5f6c218fedf042129f3582eba0d035
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-01 17:17:07 +0900

    Update 2016년.java

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -3,31 +3,28 @@ import java.time.DayOfWeek;
 
 class Solution {
     public String solution(int a, int b) {
-        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        int[] dateM = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};   // 요일
         
-        int dateY = b - 1;
+        LocalDate date = LocalDate.of(2016, a, b);   // 연, 월, 일
+        DayOfWeek dayOfWeek = date.getDayOfWeek();   // 해당 일자의 요일 (Enum)
+        int dayOfWeekNumber = dayOfWeek.getValue();  // 요일의 값 (월요일:1 ~ 일요일:7)
         
-        if (a >= 2) {
-            for (int i = 0; i < a - 1; i++) {
-            dateY += dateM[i];
-            }
-        }
-        
-        return day[dateY % 7];
-        
-        
-        
-//         String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-        
-//         LocalDate date = LocalDate.of(2016, a, b);
-//         DayOfWeek dayOfWeek = date.getDayOfWeek();
-//         int dayOfWeekNumber = dayOfWeek.getValue();
-        
-//         return day[dayOfWeekNumber - 1];
+        return day[dayOfWeekNumber - 1];
         
         
         // 단 한 줄의 코드로 해결 가능
-        // return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);        
+        // return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
+        
+        
+        // time 클래스를 사용하지 않고 직접 구현하는 방식이 처리속도면에서는 월등하다
+        // String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        // int[] dateM = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};        
+        // int dateY = b - 1;        
+        // if (a >= 2) {
+        //     for (int i = 0; i < a - 1; i++) {
+        //     dateY += dateM[i];
+        //     }
+        // }        
+        // return day[dateY % 7];
     }
 }
