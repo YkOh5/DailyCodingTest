be8328649027672b8313649379a299aff8bb286a
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-02 21:45:16 +0900

    Update NULL 처리하기.sql

--- 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,9 +1,17 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_TYPE, 
-CASE 
-WHEN NAME IS NULL THEN 'No name'
-ELSE NAME
-END
-, SEX_UPON_INTAKE
+    IFNULL(NAME, "No name") AS NAME,
+    SEX_UPON_INTAKE
 FROM ANIMAL_INS    
 ORDER BY ANIMAL_ID
+
+
+-- NAME의 값이 NULL이 아닌 공백('')인 경우도 존재한다면 조건문을 통해서 아래와 같이 처리 가능
+# SELECT ANIMAL_TYPE, 
+#     CASE 
+#         WHEN IFNULL(NAME, '') = '' THEN 'No name'
+#         ELSE NAME
+#     END,
+#     SEX_UPON_INTAKE
+# FROM ANIMAL_INS    
+# ORDER BY ANIMAL_ID
