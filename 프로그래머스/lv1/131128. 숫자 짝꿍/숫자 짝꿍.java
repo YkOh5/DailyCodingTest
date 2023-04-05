1431de899d358dd36b4e654c9a63a3f85012ebeb
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-05 09:19:01 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,31 +1,30 @@
 class Solution {
     public String solution(String X, String Y) {
+        // 주어진 문자열들에 포함된 각 숫자들의 개수를 기록하기 위한 배열
         int[] numsX = new int[10];
         int[] numsY = new int[10];
         
-        // for (int i = 0; i < X.length(); i++) numsX[X.charAt(i) - '0']++;
-        // for (int i = 0; i < Y.length(); i++) numsY[Y.charAt(i) - '0']++;
-        
-        // Character.getNumericValue();        
-        for (int i = 0; i < X.length(); i++) numsX[Character.getNumericValue(X.charAt(i))]++;
-        for (int i = 0; i < Y.length(); i++) numsY[Character.getNumericValue(Y.charAt(i))]++;
-        
-        // for each + getNV
-        // for (char num : X.toCharArray()) numsX[Character.getNumericValue(num)]++;
-        // for (char num : Y.toCharArray()) numsY[Character.getNumericValue(num)]++;
+        // 문자열에 포함된 각각의 숫자와 일치하는 인덱스에 위치한 배열의 원소를 1씩 증가
+        for (int i = 0; i < X.length(); i++) numsX[X.charAt(i) - '0']++;
+        for (int i = 0; i < Y.length(); i++) numsY[Y.charAt(i) - '0']++;
         
         StringBuilder answer = new StringBuilder();
+        
+        // 내림차순으로 두 배열을 탐색하며
         for (int i = numsX.length - 1; i >= 0; i--) {
+            // 같은 인덱스에 위치한 두 배열의 원소 중 하나라도 0이 될 때까지
             while (numsX[i] > 0 && numsY[i] > 0) {
-                answer.append(i);
+                answer.append(i);   // 해당 인덱스의 값을 StringBuilder에 추가
+                // 해당 원소값 감소
                 numsX[i]--;
                 numsY[i]--;
             }
         }
         
+        // 공통적으로 나타나는 숫자가 없는 경우
         if (answer.length() == 0) return "-1";
-        
-        if (answer.charAt(0) == '0') return "0";        
+        // 공통적으로 나타나는 숫자가 0뿐인 경우
+        if (answer.charAt(0) == '0') return "0";
         
         return answer.toString();
     }
