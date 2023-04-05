2b92c707c1b113a76e33141ca93e86cbd79aad96
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-05 10:15:13 +0900

    [level 1] Title: 실패율, Time: 0.67 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -6,11 +6,12 @@ class Solution {
         int[] challengers = new int[N + 1];   // 각 스테이지에 도전 중인 사용자 수
         double[] failureRate = new double[N]; // 각 스테이지의 실패율
         double[][] stageFailureRate = new double[N][2];   // 스테이지와 실패율을 매핑
-         
+        // StageFailureRate[] stageFailureRate = new StageFailureRate[N];
+
         for (int i = 0; i < stages.length; i++) {
             challengers[stages[i] - 1]++;
         }
-        
+
         for (int i = 0; i < N; i++) {
             if (players == 0)   // 스테이지에 도달한 사용자가 없는 경우
                 failureRate[i] = 0;   // 해당 스테이지의 실패율 = 0%
@@ -20,17 +21,30 @@ class Solution {
             // 각각의 스테이지와 실패율을 매핑
             stageFailureRate[i][0] = i + 1;
             stageFailureRate[i][1] = failureRate[i];
+            // stageFailureRate[i] = new StageFailureRate(i + 1, failureRate[i]);
         }
-        
+
         // 실패율을 기준으로 내림차순으로 정렬
         Arrays.sort(stageFailureRate, (r1, r2) -> Double.compare(r2[1], r1[1]));
-        
+        // Arrays.sort(stageFailureRate, (r1, r2) -> Double.compare(r2.failureRate, r1.failureRate));
+
         // 정렬된 스테이지의 번호들을 추출
         int[] answer = new int[N];
         for (int i = 0; i < N; i++) {
             answer[i] = (int)stageFailureRate[i][0];
+            // answer[i] = stageFailureRate[i].stage;
         }
-        
+
         return answer;
     }
 }
+
+// class StageFailureRate {
+//     int stage;
+//     double failureRate;
+    
+//     StageFailureRate(int stage, double failureRate) {
+//         this.stage = stage;
+//         this.failureRate = failureRate;
+//     }
+// }
