88aa7537d3e9c04d9a1944505fe7dd2c2003272e
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-06 14:39:09 +0900

    Update 소수 만들기.java

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -5,8 +5,8 @@ class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    // 소수 판별을 위한 메서드의 매개변수로 3개의 수를 투입
-                    if (isPrimeNumber(nums[i], nums[j], nums[k])) answer++;
+                    // 소수 판별을 위한 메서드의 매개변수로 양의 정수 3개의 합을 투입
+                    if (isPrimeNumber(nums[i] + nums[j] + nums[k])) answer++;
                 }
             }
         }
@@ -14,11 +14,11 @@ class Solution {
         return answer;
     }
     
-    // 양의 정수 3개의 합이 소수인지 판별하기 위한 메서드
-    boolean isPrimeNumber(int x, int y, int z) {
+    // 매개변수가 소수인지 판별하기 위한 메서드
+    boolean isPrimeNumber(int sum) {
         // 합의 제곱근까지만 검사함으로써 불필요한 반복 제거
-        for (int i = 2; i <= Math.sqrt(x + y + z); i++) {
-            if ((x + y + z) % i == 0) return false;
+        for (int i = 2; i <= Math.sqrt(sum); i++) {
+            if ((sum) % i == 0) return false;
         }
         return true;
     }
