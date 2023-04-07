a964ed905fe6c87e76f861d3917dcdf0a321d900
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-07 09:14:52 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,35 @@
+// 새로운 데이터를 매번 추가해야 하고, 기존의 데이터들과 값의 크기를 비교해야 하므로 ArrayList를 이용
+
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        ArrayList<Integer> hallOfFame = new ArrayList<>();   
+        int[] answer = new int[score.length];
+        int targetIdx = 0;   // 당일에 출연한 가수의 점수가 위치할 인덱스
+        
+        hallOfFame.add(score[0]);   
+        answer[0] = score[0];
+        
+        for (int i = 1; i < score.length; i++) {
+            for (int j = 0; j < hallOfFame.size(); j++) {
+                targetIdx = hallOfFame.size();
+                // 새로운 점수가 특정 인덱스에 위치한 기존의 점수보다 작으면
+                if (score[i] < hallOfFame.get(j)) {
+                    targetIdx = j;   // 해당 인덱스에 새로운 점수를 추가
+                    break;
+                }
+            }
+            
+            hallOfFame.add(targetIdx, score[i]);
+            // 명예의 전당에 올려진 점수의 개수가 k개를 초과하면
+            if (hallOfFame.size() > k) {
+                hallOfFame.remove(0);   // 최하위 점수 탈락
+            }
+            
+            answer[i] = hallOfFame.get(0);   // 상위 k번째 이내의 최하위 점수 발표
+        }
+        
+        return answer;
+    }
+}
