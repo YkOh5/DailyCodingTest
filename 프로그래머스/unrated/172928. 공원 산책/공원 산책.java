f836e59147eac455a298c5318e46e238c983a24f
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-12 07:57:54 +0900

    [unrated] Title: 공원 산책, Time: 0.05 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,74 @@
+class Solution {
+    private int[] curPos = new int[2];   // 현재 위치
+    
+    public int[] solution(String[] park, String[] routes) {
+        findStartingPoint(park);   // 출발점 찾기
+        
+        for (int i = 0; i < routes.length; i++) {
+            char op = routes[i].charAt(0);   // 이동 방향
+            int n = Character.getNumericValue(routes[i].charAt(2));   // 이동할 칸의 수
+            
+            // 이동 명령 수행
+            switch (op) {
+                case 'E' : moveEast(park, n);   // 동
+                    break;
+                case 'W' : moveWest(park, n);   // 서
+                    break;
+                case 'S' : moveSouth(park, n);  // 남
+                    break;
+                case 'N' : moveNorth(park, n);  // 북
+            }
+        }
+                
+        return curPos;
+    }
+    
+    // 출발점 찾기
+    private void findStartingPoint(String[] park) {
+        for (int i = 0; i < park.length; i++) {
+            for (int j = 0; j < park[i].length(); j++) {
+                if (park[i].charAt(j) == 'S') {
+                    curPos[0] = i;   // x좌표
+                    curPos[1] = j;   // y좌표
+                    return;
+                }
+            }
+        }
+    }
+    
+    // 동쪽으로 이동
+    private void moveEast(String[] park, int n) {
+        if (curPos[1] + n >= park[0].length()) return;   // 공원을 벗어난 경우
+        for (int j = 1; j <= n; j++) {
+            if (park[curPos[0]].charAt(curPos[1] + j) == 'X') return;   // 장애물에 가로막힌 경우
+        }                
+        curPos[1] = curPos[1] + n;
+    }
+    
+    // 서쪽으로 이동
+    private void moveWest(String[] park, int n) {
+        if (curPos[1] - n < 0) return;
+        for (int j = 1; j <= n; j++) {
+            if (park[curPos[0]].charAt(curPos[1] - j) == 'X') return;
+        }                
+        curPos[1] = curPos[1] - n;
+    }
+    
+    // 남쪽으로 이동
+    private void moveSouth(String[] park, int n) {
+        if (curPos[0] + n >= park.length) return;
+        for (int j = 1; j <= n; j++) {
+            if (park[curPos[0] + j].charAt(curPos[1]) == 'X') return;
+        }                
+        curPos[0] = curPos[0] + n;
+    }
+    
+    // 북쪽으로 이동
+    private void moveNorth(String[] park, int n) {
+        if (curPos[0] - n < 0) return;
+        for (int j = 1; j <= n; j++) {
+            if (park[curPos[0] - j].charAt(curPos[1]) == 'X') return;
+        }                
+        curPos[0] = curPos[0] - n;
+    }
+}
