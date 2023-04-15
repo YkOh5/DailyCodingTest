d191563e3ebff1db536bccfbd7dc5e27c7110bfa
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-16 06:15:39 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.51 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,41 +1,51 @@
 // 엄지손가락이 상하좌우 4가지 방향으로만 이동할 수 있으므로, 유클리드 거리가 아닌 맨하탄 거리를 사용해야 한다
 
 class Solution {
+    private StringBuilder answer = new StringBuilder();
+    // 각 숫자(= numPos의 index)들의 위치
+    private int[][] numPos = {{3, 1}, {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2 ,0}, {2, 1}, {2, 2}};
+    private int[] leftPos = {3, 0};   // 왼손의 현재 위치
+    private int[] rightPos = {3, 2};   // 오른손의 현재 위치
+    
     public String solution(int[] numbers, String hand) {
-        StringBuilder answer = new StringBuilder();
-        // 각 숫자(= numPos의 index)들의 위치
-        int[][] numPos = {{3, 1}, {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2 ,0}, {2, 1}, {2, 2}};
-        int[] leftPos = {3, 0};   // 왼손의 현재 위치
-        int[] rightPos = {3, 2};   // 오른손의 현재 위치
-        
         for (int num : numbers) {
             // 해당 숫자와 양손 사이의 맨하탄 거리
-            int distanceL = Math.abs(leftPos[0] - numPos[num][0]) + Math.abs(leftPos[1] - numPos[num][1]);
-            int distanceR = Math.abs(rightPos[0] - numPos[num][0]) + Math.abs(rightPos[1] - numPos[num][1]);
-            
+            int distanceL = getManhattanDistance(leftPos, numPos[num]);
+            int distanceR = getManhattanDistance(rightPos, numPos[num]);
+                        
             switch (num) {
                 case 1, 4, 7 -> { 
-                    answer.append('L');
-                    leftPos = numPos[num];
+                    tappedWithTheLeftThumb(numPos[num]);
                 }
                 case 3, 6, 9 -> {
-                    answer.append('R');
-                    rightPos = numPos[num];
+                    tappedWithTheRightThumb(numPos[num]);
                 }
                 // 2, 5, 8, 0
                 default -> {
                     if (distanceL < distanceR || (distanceL == distanceR && hand.equals("left"))) {
-                        answer.append('L');
-                        leftPos = numPos[num];
+                        tappedWithTheLeftThumb(numPos[num]);
                     }
                     else {
-                        answer.append('R');
-                        rightPos = numPos[num];
+                        tappedWithTheRightThumb(numPos[num]);
                     }                    
                 }
             }                    
         }
                 
         return answer.toString();
+    }
+    
+    private int getManhattanDistance(int[] thumbPos, int[] numPos) {
+                return Math.abs(thumbPos[0] - numPos[0]) + Math.abs(thumbPos[1] - numPos[1]);
+    }
+    
+    private void tappedWithTheLeftThumb(int[] numPos) {        
+        answer.append('L');
+        leftPos = numPos;                
+    }
+    
+    private void tappedWithTheRightThumb(int[] numPos) {
+        answer.append('R');
+        rightPos = numPos;        
     }
 }
