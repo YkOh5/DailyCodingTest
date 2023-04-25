294cf8645f2a9640043f7a3ba7b101a9d50f081e
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-26 06:43:47 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,10 +1,22 @@
--- 코드를 입력하세요
 SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
-    CASE
-        WHEN STATUS = 'SALE' THEN '판매중'
-        WHEN STATUS = 'RESERVED' THEN '예약중'
-        ELSE '거래완료'
+    CASE STATUS   -- CASE절의 평가대상을 STATUS로 정해두면 이후의 조건식들을 더 간단히 할 수 있다
+        WHEN 'SALE' THEN '판매중'
+        WHEN 'RESERVED' THEN '예약중'
+        WHEN 'DONE' THEN '거래완료'
+        ELSE '?'
     END AS STATUS
 FROM USED_GOODS_BOARD
-WHERE CREATED_DATE LIKE '2022-10-05%'
+WHERE CREATED_DATE = '2022-10-05'
 ORDER BY BOARD_ID DESC;
+
+
+# WHERE절에서 조건식을 DATE_FORMAT()으로 쓰면, 정확성은 높아도 대용량 데이터 처리 시 처리속도가 떨어질 수 있다
+# SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+#     CASE
+#         WHEN STATUS = 'SALE' THEN '판매중'
+#         WHEN STATUS = 'RESERVED' THEN '예약중'
+#         ELSE '거래완료'
+#     END AS STATUS
+# FROM USED_GOODS_BOARD
+# WHERE DATE_FORMAT(CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
+# ORDER BY BOARD_ID DESC;
