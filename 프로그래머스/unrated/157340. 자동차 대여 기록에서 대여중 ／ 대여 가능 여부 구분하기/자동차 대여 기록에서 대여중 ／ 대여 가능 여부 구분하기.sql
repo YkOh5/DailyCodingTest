991f316d3e2af86f15b87f36893950e7328137f0
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-26 16:38:58 +0900

    [unrated] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -14,7 +14,7 @@ GROUP BY CAR_ID
 ORDER BY CAR_ID DESC;
 
 
-# MAX()를 사용해서 훨씬 간결한 코드로 해결 가능!
+# MAX()를 사용하면 서브쿼리 없이 훨씬 간결한 코드로 해결 가능!
 # -> '대여중'과 '대여 가능'이 중첩되는 경우 UniCode 값이 더 큰 '대여중'을 선택!!!
 # SELECT CAR_ID, 
 #          MAX(IF('2022-10-16' BETWEEN START_DATE AND END_DATE, '대여중', '대여 가능')) AS AVAILABILITY
