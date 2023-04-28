b6a2b53b54be544b9a856a96e7720bd9874d3000
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-04-28 20:39:43 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -3,13 +3,31 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        int h = Math.min(citations.length, citations[citations.length - 1]);
-        for (int i = h; i >= 0; i--) {
+        int hIndex = Math.min(citations.length, citations[citations.length - 1]);
+        for (int i = hIndex; i >= 0; i--) {
             if (i >= 1 && citations[citations.length - i] < i) continue;
             if (citations.length - i - 1 >= 0 && citations[citations.length - i - 1] > i) continue;
             return i;
         }
         
-        return -1;
+        return 0;
     }
 }
+
+
+// 아래의 코드가 처리속도는 조금 더 느리지만, 보다 직관적이다
+// class Solution {
+//     public int solution(int[] citations) {
+//         Arrays.sort(citations);  // 인용 횟수를 오름차순으로 정렬
+//         int hIndex = 0;
+//         for (int i = 0; i < citations.length; i++) {
+//             int cnt = citations.length - i;  // 현재 인용 횟수 이상인 논문 수
+//             // 인용 횟수가 현재 인용 횟수 이상인 논문이 cnt편 이상인 경우
+//             if (citations[i] >= cnt) {
+//                 hIndex = cnt;
+//                 break;
+//             }
+//         }
+//         return hIndex;
+//     }
+// }
