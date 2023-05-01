980a0cd116722035d63cd857b857b030b7004a93
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-01 16:04:50 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -8,11 +8,11 @@ class Solution {
         
         for (int i = 1; i * i <= brown + yellow; i++) {
             // 카펫의 가로와 세로의 길이가 같은 경우
-            if (i * i == brown + yellow &&  (i - 2) * (i - 2) == yellow) {
+            if (i * i == brown + yellow && (i - 2) * (i - 2) == yellow) {
                 answer[0] = answer [1] = i;
             }
             // 카펫의 가로와 세로의 길이가 다른 경우
-            else if ((brown + yellow) % i == 0 &&  (i - 2) * ((brown + yellow) / i -  2) == yellow) {
+            else if ((brown + yellow) % i == 0 && (i - 2) * ((brown + yellow) / i -  2) == yellow) {
                 answer[0] = (brown + yellow) / i;
                 answer[1] = i;
             }
