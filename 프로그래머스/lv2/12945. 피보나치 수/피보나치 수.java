db356583860bca402518e0d3c814506ad82c07c0
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-03 18:16:59 +0900

    [level 2] Title: 피보나치 수, Time: 2.31 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,9 +1,11 @@
+// 배열을 이용한 다이나믹 프로그래밍으로 시간복잡도를 O(n)으로 줄일 수 있다
+
 class Solution {
-    public long solution(int n) {
-        long[] fib = new long[n + 1];
-        fib[2] = fib[1] = 1;
+    public int solution(int n) {
+        int[] fib = new int[n + 1];
+        fib[1] = 1;
         
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567;
         }        
         
