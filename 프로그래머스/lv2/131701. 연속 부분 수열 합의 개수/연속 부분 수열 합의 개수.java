2143ae5299c0bf2c2a4719c841e0be8f581d8fab
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-04 22:36:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1424.60 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,24 +1,19 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        int n = elements.length;
-        int[] circularElements = new int[elements.length * 2];
-        for (int i = 0; i < elements.length; i++) {
-            circularElements[i + elements.length] = circularElements[i] = elements[i];
-        }
-        
-        HashSet<Integer> sumSet = new HashSet<>();        
-        for (int i = 1; i <= elements.length; i++) {
-            for (int j = 0; j < elements.length; j++) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i=1; i<=elements.length; i++) {
+            for (int j=0; j<elements.length; j++) {
                 int sum = 0;
-                for (int k = 0; k < i; k++) {
-                    sum += circularElements[j + k];
+                for (int k=j; k<j+i; k++) {
+                    sum += elements[k%elements.length];
                 }
-                sumSet.add(sum);
+                set.add(sum);
             }
-        }   
-        
-        return sumSet.size();
+        }
+
+        return set.size();
     }
 }
