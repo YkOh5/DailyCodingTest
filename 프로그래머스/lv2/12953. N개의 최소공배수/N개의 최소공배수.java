db43c27312c241446b206f95c6dbc20a58dfcd52
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-04 11:49:13 +0900

    [level 2] Title: N개의 최소공배수, Time: 52.54 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,22 +1,59 @@
+// 최소공배수 = arr의 모든 원소들의 곱(공배수)의 약수들 중에서 각 숫자로 나눠떨어지는 가장 작은 수
+
 class Solution {
     public int solution(int[] arr) {
-        long product = 1;
+        long product = 1;   // 모든 원소들의 곱(공배수)
         for (int num : arr) {
             product *= num;
         }
         
-        for (int i = 1; i <= product; i++) {
-            boolean chk = true;
-            if (product % i != 0) continue;
-            for (int num : arr) {
-                if (i % num != 0) {
-                    chk = false;
+        for (int i = 1; i <= product; i++) {   // 모든 원소들의 곱의
+            if (product % i != 0) continue;   // 약수들 중에서
+            
+            boolean isLCM = true;
+            for (int num : arr) {   // 각 숫자들로
+                if (i % num != 0) {   // 나눠떨어지지 않는 경우
+                    isLCM = false;
                     break;
                 }
             }
-            if (chk == true) return i;
+            
+            if (isLCM == true) return i;   // 나눠떨어지는 경우
         }
         
-        return 1;
+        return -1;
     }
 }
+
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// class Solution {
+//     public int solution(int[] arr) {
+//         long product = 1;
+//         for (int num : arr) {
+//             product *= num;
+//         }        
+//         ArrayList<Long> list = new ArrayList<>();
+//         int idx = 0;
+//         for (long i = 1; i * i <= product; i++) {
+//             if (product % i == 0) {
+//                 list.add(idx, i);                
+//                 list.add(list.size() - idx, product / i);
+//                 idx++;
+//             }
+//         }        
+//         for (long divisor : list) {
+//             boolean isLCM = true;
+//             for (int num : arr) {
+//                 if (divisor % num != 0) {
+//                     isLCM = false;
+//                     break;
+//                 }
+//             }
+//             if (isLCM == true) return (int)divisor;
+//         }        
+//         return 1;
+//     }
+// }
