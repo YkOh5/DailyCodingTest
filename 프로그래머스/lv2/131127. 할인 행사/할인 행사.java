0571833eef79e3afa321ed70577541d47feea9df
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-05 18:11:14 +0900

    [level 2] Title: 할인 행사, Time: 11.71 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -1,31 +1,45 @@
-import java.util.HashMap;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        HashMap<String, Integer> itemMap = new HashMap<String, Integer>();
-        for (int i = 0; i < want.length; i++) {
-            itemMap.put(want[i], number[i]);
+        Customer customer = new Customer(IntStream.range(0, want.length)
+                .mapToObj(i -> new AbstractMap.SimpleEntry<>(want[i], number[i]))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+
+
+        return customer.calculateSignUpDates(Arrays.stream(discount).collect(Collectors.toList()), 10);
+    }
+
+    private static class Customer {
+        private final Map<String, Integer> want;
+
+        public Customer(Map<String, Integer> want) {
+            this.want = want;
         }
 
-        int cnt = 0;
-        for (int i = 0; i < discount.length; i++) {
-            HashMap<String, Integer> clonedMap = (HashMap<String, Integer>)itemMap.clone();   
-            for (int j = i; j < i + 10; j++) {
-                if (j >= discount.length) continue;
-                clonedMap.put(discount[j], clonedMap.getOrDefault(discount[j], 0) - 1);
+        public int calculateSignUpDates(List<String> products, int timeWindow) {
+            if (products.size() <= timeWindow) {
+                return canSignUp(products) ? 1 : 0;
             }
 
-            boolean chk = true;
-            for (int count : clonedMap.values()) {
-                if (count > 0) {
-                    chk = false;
-                    break;
-                }
-            }
-
-            if (chk) cnt++;
+            return (int) IntStream.rangeClosed(0, products.size() - timeWindow)
+                    .filter(i -> canSignUp(products.subList(i, i + timeWindow)))
+                    .count();
         }
 
-        return cnt;
+        private boolean canSignUp(List<String> products) {
+            return products.stream()
+                    .collect(Collectors.collectingAndThen(
+                            Collectors.groupingBy(Function.identity(), Collectors.counting()),
+                            groupedProducts -> want.entrySet().stream()
+                                    .allMatch(entry ->
+                                            groupedProducts.getOrDefault(entry.getKey(), 0L) >= entry.getValue())));
+        }
     }
 }
