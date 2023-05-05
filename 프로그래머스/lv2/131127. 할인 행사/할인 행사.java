583b188f33b5fe1f48bc9d5c6047035ed6e6395a
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-05 16:56:34 +0900

    [level 2] Title: 할인 행사, Time: 0.11 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,34 @@
+
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        HashMap<String, Integer> itemMap = new HashMap<String, Integer>();
+        int sum = 0;
+        for (int i = 0; i < want.length; i++) {
+            itemMap.put(want[i], number[i]);
+            sum += number[i];
+        }
+        
+        int cnt = 0;
+        for (int i = 0; i < discount.length; i++) {
+            HashMap<String, Integer> clonedMap = (HashMap<String, Integer>)itemMap.clone();   
+            for (int j = i; j < i + 10; j++) {
+                if (j >= discount.length) continue;
+                clonedMap.put(discount[j], clonedMap.getOrDefault(discount[j], 0) - 1);
+            }
+            
+            boolean chk = true;
+            for (int count : clonedMap.values()) {
+                if (count > 0) {
+                    chk = false;
+                    break;
+                }
+            }
+            
+            if (chk) cnt++;
+        }
+        
+        return cnt;
+    }
+}
