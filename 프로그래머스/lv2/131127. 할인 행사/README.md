13331ad66c7bb7d24fa962579aa6ba82794fbaac
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-05 22:03:43 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/README.md
+++ 프로그래머스/lv2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 74.6 MB, 시간: 11.71 ms
+메모리: 76.4 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -1,45 +1,89 @@
-import java.util.AbstractMap;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.HashMap;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        Customer customer = new Customer(IntStream.range(0, want.length)
-                .mapToObj(i -> new AbstractMap.SimpleEntry<>(want[i], number[i]))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
-
-
-        return customer.calculateSignUpDates(Arrays.stream(discount).collect(Collectors.toList()), 10);
+        HashMap<String, Integer> discountedMap = new HashMap<>();
+        int membershipDuration = 10;
+        int cnt = 0;
+        for (int i = 0; i < membershipDuration; i++) {
+            discountedMap.put(discount[i], discountedMap.getOrDefault(discount[i], 0) + 1);
+        }
+        if(isValidPeriod(want, number, discountedMap)) cnt++;
+        
+        for (int i = membershipDuration; i < discount.length; i++) {
+            discountedMap.put(discount[i - membershipDuration], discountedMap.getOrDefault(discount[i - membershipDuration], 0) - 1);
+            discountedMap.put(discount[i], discountedMap.getOrDefault(discount[i], 0) + 1);
+            
+            if(isValidPeriod(want, number, discountedMap)) cnt++;
+        }        
+        
+        return cnt;
     }
-
-    private static class Customer {
-        private final Map<String, Integer> want;
-
-        public Customer(Map<String, Integer> want) {
-            this.want = want;
+    
+    private boolean isValidPeriod(String[] want, int[] number, HashMap<String, Integer> discountedMap) {
+        for (int i = 0; i < want.length; i++) {
+            if (discountedMap.getOrDefault(want[i], 0) < number[i]) return false;
         }
-
-        public int calculateSignUpDates(List<String> products, int timeWindow) {
-            if (products.size() <= timeWindow) {
-                return canSignUp(products) ? 1 : 0;
-            }
-
-            return (int) IntStream.rangeClosed(0, products.size() - timeWindow)
-                    .filter(i -> canSignUp(products.subList(i, i + timeWindow)))
-                    .count();
-        }
-
-        private boolean canSignUp(List<String> products) {
-            return products.stream()
-                    .collect(Collectors.collectingAndThen(
-                            Collectors.groupingBy(Function.identity(), Collectors.counting()),
-                            groupedProducts -> want.entrySet().stream()
-                                    .allMatch(entry ->
-                                            groupedProducts.getOrDefault(entry.getKey(), 0L) >= entry.getValue())));
-        }
+        
+        return true;
     }
 }
+
+
+// 아래의 코드는 불필요한 반복 때문에 처리속도가 매우 떨어진다
+// import java.util.HashMap;
+// class Solution {
+//     public int solution(String[] want, int[] number, String[] discount) {
+//         HashMap<String, Integer> itemMap = new HashMap<String, Integer>();
+//         for (int i = 0; i < want.length; i++) {
+//             itemMap.put(want[i], number[i]);
+//         }
+//         int cnt = 0;
+//         for (int i = 0; i < discount.length; i++) {
+//             HashMap<String, Integer> clonedMap = (HashMap<String, Integer>)itemMap.clone();   
+//             for (int j = i; j < i + 10; j++) {
+//                 if (j >= discount.length) continue;
+//                 clonedMap.put(discount[j], clonedMap.getOrDefault(discount[j], 0) - 1);
+//             }
+//             boolean flag = true;
+//             for (String key : itemMap.keySet()) {
+//                 if (clonedMap.get(key) > 0) {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if (flag) cnt++;
+//         }
+//         return cnt;
+//     }
+// }
+
+
+// 아래와 같은 방식의 코드가 간결하고, 처리속도도 훨씬 빠르다
+// class Solution {
+//        static public int solution(String[] want, int[] number, String[] discount) {
+//         int registratableDays = 0;   // 주어진 조건을 만족하는 회원등록이 가능한 날짜의 총 일수
+//         int membershipDuration = 10;   // 회원자격 유지 기간           
+//         // 할인기간 내에서
+//         for(int i = 0; i <= discount.length - membershipDuration; i++) {
+//             boolean isValidPeriod = true;
+//             // 구매하기를 원하는 제품과
+//             for(int j = 0; j < want.length; j++) {
+//                 int cnt = 0;
+//                 // 회원자격 유지 기간 동안
+//                 for(int k = i; k < i + membershipDuration; k++) {
+//                     // 할인하는 제품이 일치하는 경우
+//                     if(discount[k].equals(want[j])) cnt++;   // 해당 제품에 대한 구매가능 횟수 증가
+//                 }
+//                 // 원하는 제품을 모두 구매할 수 없는 경우
+//                 if(cnt < number[j]) {
+//                     isValidPeriod = false;
+//                     break;   
+//                 }            
+//             }
+//             // 구매할 수 있는 경우
+//             if(isValidPeriod) registratableDays ++;
+//         }           
+//         return registratableDays;
+//     }
+// }
