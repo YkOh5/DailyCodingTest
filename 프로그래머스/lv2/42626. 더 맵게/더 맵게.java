be123b41e879e1b454c1ca663a2008ccc0c003d3
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-09 14:01:34 +0900

    [level 2] Title: 더 맵게, Time: 1864.72 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,3 +1,5 @@
+// PriorityQueue에 스코빌 지수를 추가하는 방식은 매번 시간복잡도가 O(logN)인 Heap을 사용하므로, 총 시간복잡도 = O(N*logN)
+
 import java.util.PriorityQueue;
 
 class Solution {
@@ -15,3 +17,29 @@ class Solution {
         return (scovilleScale.peek() >= K)? cnt : -1;
     }
 }
+
+
+// List에 스코빌 지수를 추가하는 방식은 매번 시간복잡도가 O(N*logN)인 정렬을 반복해야 하므로, 총 시간복잡도 = O(N^2*logN)
+// PriorityQueue를 사용한 방식에 비해 훨씬 처리속도가 떨어지므로, 효율성 테스트 실패
+
+// import java.util.ArrayList;
+// import java.util.Collections;
+
+// class Solution {
+//     public int solution(int[] scoville, int K) {
+//         ArrayList<Integer> scovilleScale = new ArrayList<>();
+//         for (int scale : scoville) scovilleScale.add(scale);
+        
+//         int cnt = 0;
+//         Collections.sort(scovilleScale);
+//         while (scovilleScale.get(0) < K && scovilleScale.size() >= 2) {
+//             scovilleScale.add(scovilleScale.get(0) + scovilleScale.get(1) * 2);
+//             scovilleScale.remove(0);
+//             scovilleScale.remove(0);
+//             cnt++;
+//             Collections.sort(scovilleScale);
+//         } 
+
+//         return (scovilleScale.get(0) >= K)? cnt : -1;
+//     }
+// }
