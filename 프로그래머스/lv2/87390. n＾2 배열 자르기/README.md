477c5a4a4a2c7d47a60a015abc46da0b7610c6a9
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-12 18:14:02 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.39 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#) 
 
 ### 성능 요약
 
-메모리: 103 MB, 시간: 6.34 ms
+메모리: 92.5 MB, 시간: 6.39 ms
 
 ### 구분
 
--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -14,3 +14,25 @@ class Solution {
         return answer;
     }
 }
+
+
+// 조건에 따라 배열 전체를 생성해서 주어진 구간을 발췌하는 방식은 매우 비효율적
+// class Solution {
+//     public long[] solution(int n, long left, long right) {        
+//         int[] nums = new int[n * n];        
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 nums[n * i + j] = j + 1;
+//             }            
+//             for (int k = 0; k < i; k++) {
+//                 nums[n * i + k] = i + 1;
+//             }
+//         }                
+//         long[] answer = new long[(int)(right - left + 1)];
+//         int idx = 0;
+//         for (long i = left; i <= right; i++) {
+//             answer[idx++] = nums[(int)i];
+//         }        
+//         return answer;
+//     }
+// }
