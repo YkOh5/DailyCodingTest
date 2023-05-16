7320caa5a0dff3fe1191edb3a7cc43ddcf8c65db
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-17 00:40:25 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.18 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -6,25 +6,9 @@ class Solution {
         return (int)(getJaccardIndex(str1, str2) * 65536);
     }
     
-    private List<String> getMultiSet(String str) {
-        char[] charArr = str.toLowerCase().toCharArray();   
-        
-        List<String> list = new ArrayList<>();        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < charArr.length - 1; i++) {
-            if (Character.isLetter(charArr[i]) && Character.isLetter(charArr[i + 1])) {
-                sb.setLength(0);
-                sb.append(charArr[i]);
-                sb.append(charArr[i + 1]);
-                list.add(sb.toString());
-            }
-        }
-        
-        return list;
-    }
-    
+    // 두 개의 문자열 간 자카드 유사도 구하기
     private double getJaccardIndex(String str1, String str2) {
-        List<String> multiSet1 = getMultiSet(str1);
+        List<String> multiSet1 = getMultiSet(str1);   // 각 문자열에 대한 다중집합
         List<String> multiSet2 = getMultiSet(str2);
         
         if (multiSet1.size() > multiSet2.size()) {
@@ -33,8 +17,9 @@ class Solution {
             multiSet2 = tmp;
         }
         
-        int sizeSum = multiSet1.size() + multiSet2.size();
-        int interCnt = 0;
+        int sizeSum = multiSet1.size() + multiSet2.size();   // 두 다중집합의 모든 원소 개수
+        int interCnt = 0;   // 교집합의 원소 개수
+        
         for (int i = 0; i < multiSet1.size(); i++) {
             if (multiSet2.contains(multiSet1.get(i))) {
                 interCnt++;
@@ -42,6 +27,26 @@ class Solution {
             }
         }
         
+        // 합집합의 원소 개수 = 모든 원소 개수 - 교집합의 원소 개수
+        // 자카드 유사도 = 교집합의 원소 개수 / 합집합의 원소 개수
         return (multiSet1.size() == 0 && multiSet2.size() == 0)? 1 : (double)interCnt / (sizeSum - interCnt);
+    }
+    
+    // 입력된 문자열을 주어진 조건에 따른 다중집합으로 변환
+    private List<String> getMultiSet(String str) {
+        char[] charArr = str.toLowerCase().toCharArray();        
+        List<String> multiSet = new ArrayList<>();        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < charArr.length - 1; i++) {
+            if (Character.isLetter(charArr[i]) && Character.isLetter(charArr[i + 1])) {
+                sb.setLength(0);
+                sb.append(charArr[i]);
+                sb.append(charArr[i + 1]);
+                multiSet.add(sb.toString());
+            }
+        }
+        
+        return multiSet;
     }
 }
