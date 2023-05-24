a10a61a3e6a4ece20a82d0438d61e010753020ab
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-24 21:46:24 +0900

    [unrated] Title: 문자 리스트를 문자열로 변환하기, Time: 0.05 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
+++ 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -8,6 +8,8 @@ class Solution {
 }
 
 
+// String.join()을 사용하면 훨씬 더 간단하고 효율적으로 해결할 수 있다
+// 주어진 문자열 배열의 크기가 크면 클수록 String
 // class Solution {
 //     public String solution(String[] arr) {        
 //         return String.join("", arr);
