26cbcc3c6d2ced25eda2d2162c3d99cb569c7dc6
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-25 23:22:23 +0900

    [level 2] Title: 스킬트리, Time: 8.88 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,28 +1,18 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        ArrayList<Character> skillList = new ArrayList<Character>();
-        for (char c : skill.toCharArray()) {
-            skillList.add(c);
-        }
-        
-        int cnt = 0;
-        for (String skillTree : skill_trees) {
-            ArrayList<Character> copiedSkillList = new ArrayList<>(skillList);
-            boolean isValid = true;
-            for (char c : skillTree.toCharArray()) {
-                if (copiedSkillList.isEmpty()) break;
-                
-                if (c == copiedSkillList.get(0)) copiedSkillList.remove(0);
-                else if (copiedSkillList.contains(c)) {
-                    isValid = false;
-                    break;
-                }
+        int answer = 0;
+        ArrayList<String> skillTrees = new ArrayList<String>(Arrays.asList(skill_trees));
+        //ArrayList<String> skillTrees = new ArrayList<String>();
+        Iterator<String> it = skillTrees.iterator();
+
+        while (it.hasNext()) {
+            if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
+                it.remove();
             }
-            if (isValid) cnt++;
         }
-        
-        return cnt;
+        answer = skillTrees.size();
+        return answer;
     }
 }
