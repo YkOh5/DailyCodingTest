fd8b728e7a943210d34dbde753081b3937011e00
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-27 08:02:51 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,11 +1,25 @@
+// class Solution {
+//     public int[] solution(int n) {        
+//         int[] answer = new int[(n % 2 == 0)? n / 2 : n / 2 + 1];
+//         int idx = 0;
+//         for (int i = 1; i <= n; i += 2) {
+//             answer[idx++] = i;
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+import java.util.ArrayList;
+
 class Solution {
-    public int[] solution(int n) {        
-        int[] answer = new int[(n % 2 == 0)? n / 2 : n / 2 + 1];
-        int idx = 0;
+    public Integer[] solution(int n) {
+        ArrayList<Integer> numList = new ArrayList<>();
         for (int i = 1; i <= n; i += 2) {
-            answer[idx++] = i;
+            numList.add(i);
         }
         
-        return answer;
+        return numList.toArray(new Integer[0]);
     }
 }
