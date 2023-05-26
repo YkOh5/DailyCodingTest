c4a2f64a40b40ec54ef44d9cee53b3fb279f6444
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-26 14:17:28 +0900

    [level 2] Title: 스킬트리, Time: 0.23 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -26,3 +26,27 @@ class Solution {
         return cnt;
     }
 }
+
+
+// String.indexOf() 함수를 이용하면 더 간단하고 효율적인 코드를 작성할 수 있다
+// 해당 함수는 문자열이 특정 문자를 포함하지 않을 경우 -1을 반환한다
+// class Solution {
+//     public int solution(String pre_requisite_skills, String[] skill_trees) {
+//         int cnt = 0;        
+//         for (String skill_tree : skill_trees) {
+//             int targetIdx = 0;
+//             boolean validSkillTree = true;            
+//             for (char skill : skill_tree.toCharArray()) {
+//                 int skillIdx = pre_requisite_skills.indexOf(skill);                
+//                 if (skillIdx == -1) continue;
+//                 else if (skillIdx == targetIdx) targetIdx++;
+//                 else {
+//                     validSkillTree = false;
+//                     break;
+//                 }
+//             }            
+//             if (validSkillTree) cnt++;
+//         }        
+//         return cnt;
+//     }
+// }
