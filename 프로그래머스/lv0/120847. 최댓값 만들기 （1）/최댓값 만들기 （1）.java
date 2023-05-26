c356b10b23b70e59460ca129097aa5518aeac3d6
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-27 06:48:27 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 2.81 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,27 +1,11 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int[] numbers) {
-        int max = Integer.MIN_VALUE;
-        int secMax = Integer.MIN_VALUE;
-        
-        for (int number : numbers) {
-            if (number > max) {
-                secMax = max;
-                max = number;
-            } else if (number > secMax) {
-                secMax = number;
-            }
+        public int solution(int[] numbers) {
+
+            // reverse sort
+            numbers = Arrays.stream(numbers).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
+            int answer = numbers[0] * numbers[1];
+            return answer;
         }
-        
-        return max * secMax;
     }
-}
-
-
-// import java.util.Arrays;
-
-// class Solution {
-//     public int solution(int[] numbers) {
-//         Arrays.sort(numbers);
-//         return numbers[numbers.length - 1] * numbers[numbers.length - 2];
-//     }
-// }
