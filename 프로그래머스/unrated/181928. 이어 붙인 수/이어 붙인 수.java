d58ba47a06e27b9008eeab6074bc4cbdfd4d56a0
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-28 10:57:50 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,13 +1,28 @@
 class Solution {
     public int solution(int[] num_list) {
-        StringBuilder sbOdd = new StringBuilder();
-        StringBuilder sbEven = new StringBuilder();
+        int oddConcat = 0;
+        int evenConcat = 0;
         
         for (int num : num_list) {
-            if (num % 2 != 0) sbOdd.append(num);
-            else sbEven.append(num);
-        }        
+            if (num % 2 != 0) oddConcat = 10 * oddConcat + num;
+            else evenConcat = 10 * evenConcat + num;
+        }
         
-        return Integer.parseInt(sbOdd.toString()) + Integer.parseInt(sbEven.toString());
+        return oddConcat + evenConcat;
     }
 }
+
+
+// class Solution {
+//     public int solution(int[] num_list) {
+//         StringBuilder sbOdd = new StringBuilder();
+//         StringBuilder sbEven = new StringBuilder();
+        
+//         for (int num : num_list) {
+//             if (num % 2 != 0) sbOdd.append(num);
+//             else sbEven.append(num);
+//         }        
+        
+//         return Integer.parseInt(sbOdd.toString()) + Integer.parseInt(sbEven.toString());
+//     }
+// }
