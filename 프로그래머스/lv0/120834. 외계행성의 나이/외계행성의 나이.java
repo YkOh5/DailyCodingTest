e76b982e055dce9ad01c50f4890bcc0f2f14cfc7
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-29 22:40:46 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,14 +1,8 @@
 class Solution {
     public String solution(int age) {
-        char[] alphabets = new char[10];
-        int asciiCode = 97;
-        for (int i = 0; i < alphabets.length; i++) {
-            alphabets[i] = (char)asciiCode++;
-        }
-                
         StringBuilder sb = new StringBuilder(Integer.toString(age));
         for (int i = 0; i < sb.length(); i++) {
-            sb.setCharAt(i, alphabets[Character.getNumericValue(sb.charAt(i))]);
+            sb.setCharAt(i, (char)(Character.getNumericValue(sb.charAt(i)) + 'a'));
         }
         
         return sb.toString();
