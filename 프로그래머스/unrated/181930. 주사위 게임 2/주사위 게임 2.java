d7cecb5f7906cd79f372c495844ac14a33902d1d
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-30 07:24:25 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -3,10 +3,10 @@ class Solution {
         int type1 = a + b + c;
         int type2 = type1 * (int)(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
         int type3 = type2 * (int)(Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+                
+        if (a == b && b == c) return type3;
+        else if (a == b || b == c || c == a) return type2;
         
-        if (a != b && b != c && c != a) return type1;
-        else if (a == b && b == c) return type3;
-        
-        return type2;
+        return type1;
     }
 }
