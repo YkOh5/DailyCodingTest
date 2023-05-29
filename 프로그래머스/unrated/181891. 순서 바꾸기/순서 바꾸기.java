b05a1cdd6edfbe180a8df5299ae12541f69e0626
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-30 00:59:02 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -14,3 +14,18 @@ class Solution {
         return nums;
     }
 }
+
+
+// 좀 더 고민을 했다면 더 좋은 코드를 쓸 수 있었을텐데...
+// class Solution {
+//     public int[] solution(int[] num_list, int n) {
+//         int[] nums = new int[num_list.length];
+//         int idx = 0;
+        
+//         for (int i = 0; i < num_list.length; i++) {
+//             nums[idx++] = num_list[(i + n) % num_list.length];
+//         }
+        
+//         return nums;
+//     }
+// }
