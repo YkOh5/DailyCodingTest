c8d22a4932e210b916f8303960b948ebfca314de
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-29 16:47:01 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -10,3 +10,16 @@ class Solution {
         return max;
     }
 }
+
+
+// 정렬을 이용한 방식은 코드 자체는 더 간단하지만 처리속도가 느리다
+// import java.util.Arrays;
+
+// class Solution {
+//     public int solution(int[] numbers) {
+//         Arrays.sort(numbers);
+//         int len = numbers.length;
+        
+//         return Math.max(numbers[0] * numbers[1], numbers[len - 2] * numbers[len - 1]);
+//     }
+// }
