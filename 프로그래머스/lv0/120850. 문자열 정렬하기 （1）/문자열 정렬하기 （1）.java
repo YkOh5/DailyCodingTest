3607625ff610e25e98fb440aa6327f2991c2fc06
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-29 18:19:51 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.24 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Solution {
+    public Integer[] solution(String my_string) {
+        ArrayList<Integer> numList = new ArrayList<>();
+        for (char c : my_string.toCharArray()) {
+            if (Character.isDigit(c)) numList.add(Character.getNumericValue(c));
+        }
+        
+        numList.sort(null);
+        
+        return numList.toArray(new Integer[0]);
+    }
+}
