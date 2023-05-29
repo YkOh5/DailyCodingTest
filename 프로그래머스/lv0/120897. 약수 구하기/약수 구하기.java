6653958ac0cac5b4ade093b51cb9965ccb7005bc
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-30 03:21:51 +0900

    [level 0] Title: 약수 구하기, Time: 0.30 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -3,11 +3,14 @@ import java.util.ArrayList;
 class Solution {
     public Integer[] solution(int n) {
         ArrayList<Integer> divisorList = new ArrayList<>();
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0) divisorList.add(i);
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                divisorList.add(i);
+                if (Math.sqrt(n) != i) divisorList.add(n / i);
+            }
         }
         
-        divisorList.add(n);
+        divisorList.sort(null);
         
         return divisorList.toArray(new Integer[0]);
     }
