1df365afe7f32db84782c65df09fe15518d5b2c9
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-29 19:12:49 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.46 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,6 +1,9 @@
 class Solution {
     public String[] solution(String my_string) {
         return my_string.trim().replaceAll("\\s+", " ").split(" ");
+        
+        // 더 간단히 해결할 수 있다
         // return my_string.trim().split("\\s+");
+        // return my_string.trim().split("[ ]+");
     }
 }
