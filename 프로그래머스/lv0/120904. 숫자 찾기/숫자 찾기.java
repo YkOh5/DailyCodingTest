7952b56b8981f0edbaa1da9594c2757ba787d14f
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-30 06:59:11 +0900

    [level 0] Title: 숫자 찾기, Time: 0.11 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num, int k) {
+        char[] nums = String.valueOf(num).toCharArray();
+        for (int i = 0; i < nums.length; i++) {
+            if (Character.getNumericValue(nums[i]) == k) return i + 1;
+        }
+        
+        return -1;
+    }
+}
