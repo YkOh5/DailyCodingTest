6fcfb966253404a5d4365df9e904b0288bcd9f4b
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-29 18:21:37 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.57 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,14 +1,18 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-    public Integer[] solution(String my_string) {
-        ArrayList<Integer> numList = new ArrayList<>();
-        for (char c : my_string.toCharArray()) {
-            if (Character.isDigit(c)) numList.add(Character.getNumericValue(c));
+    public int[] solution(String my_string) {
+
+        my_string = my_string.replaceAll("[a-z]","");
+
+        int[] answer = new int[my_string.length()];
+
+        for(int i =0; i<my_string.length(); i++){
+            answer[i] = my_string.charAt(i) - '0';
         }
-        
-        numList.sort(null);
-        
-        return numList.toArray(new Integer[0]);
+
+        Arrays.sort(answer);
+
+        return answer;
     }
 }
