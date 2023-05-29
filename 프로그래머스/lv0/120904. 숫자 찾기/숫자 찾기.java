a43f2302dc6a1f217c99542125de8c4a37feeb0a
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-05-30 07:06:07 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int num, int k) {
-//         char[] nums = String.valueOf(num).toCharArray();
-//         for (int i = 0; i < nums.length; i++) {
-//             if (Character.getNumericValue(nums[i]) == k) return i + 1;
-//         }
+        char[] nums = String.valueOf(num).toCharArray();
+        for (int i = 0; i < nums.length; i++) {
+            if (Character.getNumericValue(nums[i]) == k) return i + 1;
+        }
         
-//         return -1;
+        return -1;
         
-        return ("a" + num).indexOf(Integer.toString(k));
+        // return ("a" + num).indexOf(Integer.toString(k));
     }
 }
