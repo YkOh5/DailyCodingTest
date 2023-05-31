4b988ebcf0f0b0b3a7494d717af4b9c4cacbf21f
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-01 04:00:30 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.15 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,20 +1,20 @@
 import java.util.Arrays;
 
-// class Solution {
-//     public String solution(String my_string) {
-//         String[] strArr = my_string.toLowerCase().split("");
-//         Arrays.sort(strArr);
-        
-//         return String.join("", strArr);
-//     }
-// }
-
-
 class Solution {
     public String solution(String my_string) {
-        char[] charArr = my_string.toLowerCase().toCharArray();
-        Arrays.sort(charArr);
+        String[] strArr = my_string.toLowerCase().split("");
+        Arrays.sort(strArr);
         
-        return new String(charArr);
+        return String.join("", strArr);
     }
 }
+
+
+// class Solution {
+//     public String solution(String my_string) {
+//         char[] charArr = my_string.toLowerCase().toCharArray();
+//         Arrays.sort(charArr);
+        
+//         return new String(charArr);
+//     }
+// }
