9ab17badf77036a5c10285155bb65bfbd8db31f3
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-01 04:46:19 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -4,11 +4,15 @@ class Solution {
         
         for (int i = 1; i <= n; i++) {
             int divisorCnt = 2;            
-            for (int j = 2; j <= i / 2; j++) {
-                if (i % j == 0) divisorCnt += 2; 
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (j == Math.sqrt(i)) divisorCnt++;
+                else if (i % j == 0) divisorCnt += 2;
+                
+                if (divisorCnt >= 3) {
+                    answer++;
+                    break;
+                }
             }
-            
-            if (divisorCnt >= 3) answer++;
         }
         
         return answer;
