6c1958c5940fcf539e9c54f001ad015d7eaccf8c
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-01 17:46:11 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.14 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -12,3 +12,39 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+
+// import java.util.Arrays;
+// import java.util.LinkedHashSet;
+// import java.util.List;
+
+// class Solution {
+//     public String solution(String my_string) {
+//         return String.join("", new LinkedHashSet<String>(Arrays.asList(my_string.split(""))));
+//     }
+// }
+
+
+
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public String solution(String my_string) {
+//         return my_string.chars()
+//                 .mapToObj(Character::toString)
+//                 .distinct()
+//                 .collect(Collectors.joining());
+//     }
+// }
+
+
+
+// import java.util.*;
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public String solution(String myString) {
+//         return Arrays.stream(myString.split("")).distinct().collect(Collectors.joining());
+//     }
+// }
