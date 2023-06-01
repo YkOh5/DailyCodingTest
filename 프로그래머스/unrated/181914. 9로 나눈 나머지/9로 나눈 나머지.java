958ba0f99cc086500efd030ec50e84ab489425e8
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-02 08:20:20 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.70 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -2,6 +2,6 @@ import java.math.BigInteger;
 
 class Solution {
     public int solution(String number) {
-        return Integer.parseInt(new BigInteger(number).remainder(new BigInteger("9")).toString());
+        return new BigInteger(number).remainder(new BigInteger("9")).intValue();
     }
 }
