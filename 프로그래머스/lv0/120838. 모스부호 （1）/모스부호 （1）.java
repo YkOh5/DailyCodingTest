fe1d6ae416e28d3698ebcd619387ac51dd86ffdc
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-03 05:23:46 +0900

    [level 0] Title: 모스부호 (1), Time: 9.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,31 +1,15 @@
-import java.util.HashMap;
-
 class Solution {
-    private HashMap<String, String> morseMap;
-    private String morse = "'.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f','--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l','--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r','...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x','-.--':'y','--..':'z'";
-    
     public String solution(String letter) {
-        morseMap = buildMorseMap(morse);
-        
-        return decode(letter);
-    }
-    
-    private HashMap<String, String> buildMorseMap(String morse) {
-        HashMap<String, String> morseMap = new HashMap<>();
-        for (String code : morse.replace("'", "").split(",")) {
-            String[] dotAndAlphabet = code.split(":");
-            morseMap.put(dotAndAlphabet[0], dotAndAlphabet[1]);
+        String answer = "";
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] word = letter.split(" ");
+
+        for(int i=0;i<word.length;i++){
+            for(int j=0;j<morse.length;j++){
+                if(word[i].equals(morse[j])) answer+=(char)(j+97);
+            }
         }
-        
-        return morseMap;
-    }
-    
-    private String decode(String letter) {
-        StringBuilder sb = new StringBuilder();
-        for (String ditDah : letter.split(" ")) {
-            sb.append(morseMap.get(ditDah));
-        }
-        
-        return sb.toString();
+
+        return answer;
     }
 }
