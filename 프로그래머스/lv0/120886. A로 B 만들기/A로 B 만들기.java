b54ec88048224faf994e9d4d88b76595e2579d14
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-03 20:42:02 +0900

    [level 0] Title: A로 B 만들기, Time: 0.13 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(String before, String after) {
+        ArrayList<Character> charList = new ArrayList<>();
+        for (char c : before.toCharArray()) charList.add(c);
+        
+        for (char c : after.toCharArray()) {
+            if (!charList.contains(c)) return 0;
+            else charList.remove(charList.indexOf(c));
+            // else charList.remove(Character.valueOf(c));
+        }
+        
+        return 1;
+    }
+}
