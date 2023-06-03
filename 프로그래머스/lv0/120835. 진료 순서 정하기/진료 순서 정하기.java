c3858f57aa014efa50af4100d6d181e92417230c
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-04 05:36:16 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.36 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,19 +1,20 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 class Solution {
     public int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
-
-        for(int i = 0; i < answer.length; i++){
-            if(answer[i] != 0){
-                continue;
-            }
-            int idx = 1;
-            for(int j = 0; j < answer.length; j++){
-                if(emergency[i] < emergency[j]){
-                    idx++;
-                }
-            }
-            answer[i] = idx;
+        HashMap<Integer, Integer> emergencyMap = new HashMap<>();
+        int[] levels = emergency.clone();
+        Arrays.sort(levels);
+        
+        for (int i = 0; i < levels.length; i++) {
+            emergencyMap.put(levels[i], levels.length - i);
         }
-        return answer;
+        
+        for (int i = 0; i < emergency.length; i++) {
+            emergency[i] = emergencyMap.get(emergency[i]);
+        }
+                
+        return emergency;
     }
 }
