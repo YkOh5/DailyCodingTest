36ae5a49aac7688b5feb8e38f062cf83eedaa373
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-04 00:59:40 +0900

    [level 0] Title: k의 개수, Time: 41.32 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,12 +1,24 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int cnt = 0;
+        StringBuilder sb = new StringBuilder();
         for (int num = i; num <= j; num++) {
-            for (char c : String.valueOf(num).toCharArray()) {
-                if (Character.getNumericValue(c) == k) cnt++;
-            }
+            sb.append(num);
         }
         
-        return cnt;
+        return sb.toString().replaceAll("[^" + k + "]", "").length();
     }
 }
+
+
+// class Solution {
+//     public int solution(int i, int j, int k) {
+//         int cnt = 0;
+//         for (int num = i; num <= j; num++) {
+//             for (char c : String.valueOf(num).toCharArray()) {
+//                 if (Character.getNumericValue(c) == k) cnt++;
+//             }
+//         }
+        
+//         return cnt;
+//     }
+// }
