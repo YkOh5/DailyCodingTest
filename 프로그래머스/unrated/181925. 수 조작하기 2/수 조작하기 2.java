009fafa35bc054d570e3e2b2f604b7bd939f5a2c
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-04 01:57:55 +0900

    [unrated] Title: 수 조작하기 2, Time: 4.34 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        for (int idx = 1; idx < numLog.length; idx++) {
+            int diff = numLog[idx] - numLog[idx - 1];            
+            switch (diff) {
+                case 1 -> sb.append('w');
+                case -1 -> sb.append('s');
+                case 10 -> sb.append('d');
+                case -10 -> sb.append('a');
+            }
+        }
+        
+        return sb.toString();
+    }
+}
