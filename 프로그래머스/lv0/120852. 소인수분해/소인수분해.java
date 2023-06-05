1f4f2f8ec3d0aa38a0c8f11e4a17096bd94edc0e
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-05 20:03:21 +0900

    [level 0] Title: 소인수분해, Time: 0.19 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -1,20 +1,21 @@
-import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 class Solution {
     public Integer[] solution(int n) {
-        ArrayList<Integer> primeList = new ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            if (n % i == 0 && isPrime(i)) primeList.add(i);
+        LinkedHashSet<Integer> primeNumbers = new LinkedHashSet<>();
+
+        int i = 2;
+        while (n != 0 && i <= n) {
+            if (n % i == 0) {
+                primeNumbers.add(i);
+                n /= i;
+            } else {
+                i++;
+            }
         }
-        
-        return primeList.toArray(new Integer[0]);
-    }
-    
-    private boolean isPrime(int number) {
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
-        }
-        
-        return true;
+
+//        System.out.println(primeNumbers);
+
+        return primeNumbers.toArray(new Integer[0]);
     }
 }
