3ddc89470e105759469b200d1672dc28d2929c25
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-07 20:58:39 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,28 +1,32 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] arr) {
-        return Arrays.stream(arr).map(i -> cntAction(i)).max().getAsInt();
+        int count = 0;
+        Map<Integer, int[]> map = new HashMap<>();
+        map.put(0, arr);
+        while (true) {
+            int[] temp = map.get(map.size() - 1).clone();
+
+            for (int i = 0;i < temp.length;i++) {
+                if (temp[i] >= 50 && temp[i] % 2 == 0)
+                    temp[i] = temp[i] / 2;
+                else if (temp[i] < 50 && temp[i] % 2 == 1)
+                    temp[i] = temp[i] * 2 + 1;
+            }
+
+            map.put(++count, temp);
+            if (isCheck(map.get(count - 1), map.get(count)))
+                break;
+
+        }
+        return count - 1;
     }
 
-    private int cntAction(int n) {
-
-        int cnt = 0;
-        boolean flag = true;
-
-        while (flag) {
-
-            if (n % 2 == 0 && 50 <= n) {
-                n /= 2;
-                cnt++;
-            } else if (n % 2 == 1 && n < 50) {
-                n *= 2;
-                n += 1;
-                cnt++;
-            } else {
-                flag = false;
-            }
-        }
-        return cnt;
+    public boolean isCheck(int[] arr1, int[] arr2) {
+        for (int i = 0;i < arr1.length;i++)
+            if (arr1[i] != arr2[i])
+                return false;
+        return true;
     }
 }
