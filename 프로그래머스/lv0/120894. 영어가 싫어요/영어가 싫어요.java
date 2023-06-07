30cc57a66de904938e18e1cbbc4d6debbf5f0e05
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-08 01:27:53 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.11 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,15 +1,28 @@
 class Solution {
     public long solution(String numbers) {
-        return Long.parseLong(numbers.replace("one", "1")
-                              .replace("one", "1")
-                              .replace("two", "2")
-                              .replace("three", "3")
-                              .replace("four", "4")
-                              .replace("five", "5")
-                              .replace("six", "6")
-                              .replace("seven", "7")
-                              .replace("eight", "8")
-                              .replace("nine", "9")
-                              .replace("zero", "0"));
+        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < nums.length; i++) {
+            numbers = numbers.replace(nums[i], String.valueOf(i));
+        }
+        
+        return Long.parseLong(numbers);
     }
 }
+
+
+
+// class Solution {
+//     public long solution(String numbers) {
+//         return Long.parseLong(numbers.replace("one", "1")
+//                               .replace("one", "1")
+//                               .replace("two", "2")
+//                               .replace("three", "3")
+//                               .replace("four", "4")
+//                               .replace("five", "5")
+//                               .replace("six", "6")
+//                               .replace("seven", "7")
+//                               .replace("eight", "8")
+//                               .replace("nine", "9")
+//                               .replace("zero", "0"));
+//     }
+// }
