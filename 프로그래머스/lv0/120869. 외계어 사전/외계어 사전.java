892eaac16f39bba07a147ee5d0f3f8580869fbdc
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-12 06:36:02 +0900

    [level 0] Title: 외계어 사전, Time: 0.15 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -2,26 +2,26 @@ import java.util.HashMap;
 
 class Solution {
     private HashMap<Character, Integer> letterMap;
-    
+
     public int solution(String[] alphabet, String[] dic) {
         for (String word : dic) {
             boolean theWordIs = true;
             letterMap = new HashMap<>();
-            
+
             for (char c : word.toCharArray()) {
                 letterMap.put(c, letterMap.getOrDefault(c, 0) + 1);
             }
-            
+
             for (String letter : alphabet) {
                 if (letterMap.getOrDefault(letter.charAt(0), 0) != 1) {
                     theWordIs = false;
                     break;
                 }
             }
-            
+
             if (theWordIs) return 1;
         }
-        
+
         return 2;
     }
 }
