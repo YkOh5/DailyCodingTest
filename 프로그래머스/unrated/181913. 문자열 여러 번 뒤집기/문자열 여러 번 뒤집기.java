ea9e3945d4de61c49c23104b1814aae0917bf257
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-15 02:15:24 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 3.01 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,15 +1,50 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
         StringBuilder sb = new StringBuilder(my_string);
-        for (int[] query : queries) {            
+        for (int[] query : queries) {
             my_string = sb.toString();
-            sb.setLength(0);
-            StringBuilder subSb = new StringBuilder(my_string.substring(query[0], query[1] + 1)).reverse();
-            sb.append(my_string.substring(0, query[0]));
-            sb.append(subSb);
-            sb.append(my_string.substring(query[1] + 1, my_string.length()));            
+            String revStr = new StringBuilder(my_string.substring(query[0], query[1] + 1)).reverse().toString();
+            sb.replace(query[0], query[1] + 1, revStr);
         }
         
         return sb.toString();
     }
+    
+    
 }
+
+
+
+// class Solution {
+//     public String solution(String my_string, int[][] queries) {
+//         StringBuilder sb = new StringBuilder(my_string);
+//         for (int[] query : queries) {            
+//             my_string = sb.toString();
+//             sb.setLength(0);
+//             StringBuilder subSb = new StringBuilder(my_string.substring(query[0], query[1] + 1)).reverse();
+//             sb.append(my_string.substring(0, query[0]));
+//             sb.append(subSb);
+//             sb.append(my_string.substring(query[1] + 1, my_string.length()));            
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
+
+
+// class Solution {
+//     public String solution(String my_string, int[][] queries) {
+//         StringBuilder sb = new StringBuilder(my_string);
+//         for (int[] query : queries) {            
+//             my_string = sb.toString();
+//             sb.setLength(0);
+//             StringBuilder subSb = new StringBuilder(my_string.substring(query[0], query[1] + 1)).reverse();
+//             sb.append(my_string.substring(0, query[0]));
+//             sb.append(subSb);
+//             sb.append(my_string.substring(query[1] + 1, my_string.length()));            
+//         }
+
+//         return sb.toString();
+//     }
+// }
