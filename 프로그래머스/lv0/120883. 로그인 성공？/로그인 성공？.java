698bd76483a907c0895f15a3f0f8e4b2c00b79ab
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-16 10:54:01 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        for (String[] data : db) {
+            if (data[0].equals(id_pw[0])) {
+                if (data[1].equals(id_pw[1])) return "login";
+                else return "wrong pw";
+            }
+        }
+        
+        return "fail";
+    }
+}
