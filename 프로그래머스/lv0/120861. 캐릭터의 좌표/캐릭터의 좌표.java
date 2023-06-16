30d2477e0be7050445ddaa744166b68b3625fddf
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-16 10:19:47 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,18 +1,14 @@
 class Solution {
     private int x = 0;
     private int y = 0;
-    private int limitX, limitY;
     
-    public int[] solution(String[] keys, int[] board) {
-        limitX = (board[0] - 1) / 2;
-        limitY = (board[1] - 1) / 2;
-        
+    public int[] solution(String[] keys, int[] board) {        
         for (String key : keys) {
             switch (key) {
-                case "up" -> y = (Math.abs(y + 1) <= limitY)? y + 1 : y;
-                case "down" -> y = (Math.abs(y - 1) <= limitY)? y - 1 : y;
-                case "left" -> x = (Math.abs(x - 1) <= limitX)? x - 1 : x;
-                case "right" -> x = (Math.abs(x + 1) <= limitX)? x + 1 : x;
+                case "up" -> y = (Math.abs(y + 1) <= board[1] / 2)? y + 1 : y;
+                case "down" -> y = (Math.abs(y - 1) <= board[1] / 2)? y - 1 : y;
+                case "left" -> x = (Math.abs(x - 1) <= board[0] / 2)? x - 1 : x;
+                case "right" -> x = (Math.abs(x + 1) <= board[0] / 2)? x + 1 : x;
             }
         }
                 
