b44df6249f83f4842d1efa8ec1a4fddcba04daeb
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-23 22:51:31 +0900

    [level 0] Title: 등수 매기기, Time: 0.37 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,49 +1,16 @@
-import java.util.Arrays;
-import java.util.HashMap;
-
+import java.util.*;
 class Solution {
-    private HashMap<Double, Integer> meanMap = new HashMap<>();
-    private HashMap<Double, Integer> rankMap = new HashMap<>();
-    private double[] means;
-    private double[] distinctMeans;
-    
-    public int[] solution(int[][] scores) {
-        getMeanMap(scores);
-        getRankMap(meanMap);
-        
-        int[] answer = new int[scores.length];
-        for (int i = 0; i < means.length; i++) {
-            answer[i] = rankMap.get(means[i]);
+    public int[] solution(int[][] score) {
+        List<Integer> scoreList = new ArrayList<>();
+        for(int[] t : score){
+            scoreList.add(t[0] + t[1]);
         }
-        
+        scoreList.sort(Comparator.reverseOrder());
+
+        int[] answer = new int[score.length];
+        for(int i=0; i<score.length; i++){
+            answer[i] = scoreList.indexOf(score[i][0] + score[i][1])+1;
+        }
         return answer;
-    }
-    
-    private void getMeanMap(int[][] scores) {
-        means = new double[scores.length];
-        for (int idx = 0; idx < scores.length; idx++) {
-            double sum = 0;
-            for (int score : scores[idx]) {
-                sum += score;
-            }
-            means[idx] = sum / 2;
-            meanMap.put(means[idx], meanMap.getOrDefault(means[idx], 0) + 1);
-        }
-    }
-    
-    private void getRankMap(HashMap<Double, Integer> meanMap) {
-        distinctMeans = new double[meanMap.keySet().size()];
-        int idx = 0;
-        for (Double key : meanMap.keySet()) {
-            distinctMeans[idx++] = key;
-        }
-        Arrays.sort(distinctMeans);
-        
-        int sum = 1;
-        rankMap.put(distinctMeans[distinctMeans.length - 1], 1);
-        for (int i = distinctMeans.length - 2; i >= 0; i--) {
-            sum += meanMap.getOrDefault(distinctMeans[i + 1], 0);
-            rankMap.put(distinctMeans[i], sum);
-        }
     }
 }
