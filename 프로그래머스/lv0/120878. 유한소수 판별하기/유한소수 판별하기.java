ce10423dad1ab4f255ce8eccbf32cdbd51abbc5c
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-25 07:05:19 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,27 +1,20 @@
 class Solution {
     public int solution(int a, int b) {
-        if (a % b == 0) return 1;
-        int reducedDenominator = b / getGCD(a, b);
-        for (int i = 2; i <= reducedDenominator; i++) {
-            if (reducedDenominator % i == 0 && isPrime(i) && i != 2 && i != 5) return 2;
+        int answer = 0;
+
+        for (;b%2 == 0;) {
+            b = b/2;
         }
-                
-        return 1;
-    }
-    
-    private int getGCD(int num1, int num2) {
-        for (int i = Math.min(num1, num2); i > 1; i --) {
-            if (num1 % i == 0 && num2 % i == 0) return i;            
+        for (;b%5 == 0;) {
+            b = b/5;
         }
-        
-        return 1;
-    }
-    
-    private boolean isPrime(int num) { 
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
+
+        if ((a/(double)b)%1 == 0) {
+            answer = 1;
+        } else {
+            answer = 2;
         }
-        
-        return true;
+
+        return answer;
     }
 }
