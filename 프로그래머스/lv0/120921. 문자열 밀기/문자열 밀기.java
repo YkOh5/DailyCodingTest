9c2c5c50fa9855806f6f81bee31db002078f561e
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-28 21:55:30 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        for (int i = 0; i < A.length(); i++) {
-            StringBuilder sb = new StringBuilder(A.substring(A.length() - i));
-            sb.append(A.substring(0, A.length() - i));
-            if (sb.toString().equals(B)) return i;
-        }
-        
-        return -1;
+        String tempB = B.repeat(2);
+        return tempB.indexOf(A);
     }
 }
