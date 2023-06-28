ace0845ea62d5eaf076d0d97ada8bd4c94716198
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-28 21:57:04 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,6 +1,20 @@
 class Solution {
     public int solution(String A, String B) {
-        String tempB = B.repeat(2);
-        return tempB.indexOf(A);
+        for (int i = 0; i < A.length(); i++) {
+            StringBuilder sb = new StringBuilder(A.substring(A.length() - i));
+            sb.append(A.substring(0, A.length() - i));
+            if (sb.toString().equals(B)) return i;
+        }
+        
+        return -1;
     }
 }
+
+
+
+// 아래와 같은 방법으로 더 간단하게 해결할 수 있다
+// class Solution {
+//     public int solution(String A, String B) {
+//         return B.repeat(2).indexOf(A);
+//     }
+// }
