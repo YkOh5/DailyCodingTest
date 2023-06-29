989bd185472aa19d88e40c9a18f19ad1c51d3723
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-30 07:18:46 +0900

    [level 0] Title: 특이한 정렬, Time: 3.28 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,20 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public Integer[] solution(int[] numlist, int n) {
-        Integer[] answer = new Integer[numlist.length];
-        for (int i = 0; i < numlist.length; i++) {
-            answer[i] = numlist[i];
-        }
-        
-        Arrays.sort(answer, (num1, num2) -> {
-            int diff1 = Math.abs(n - num1);
-            int diff2 = Math.abs(n - num2);
-            
-            if (diff1 == diff2) return num2 - num1;
-            else return diff1 - diff2;                                        
-        });
-        
-        return answer;
+    public int[] solution(int[] numlist, int n) {
+        return Arrays.stream(numlist).mapToObj(a->(Integer)a)
+            .sorted((a, b)->{
+                int u = Math.abs(n - a);
+                int v = Math.abs(n - b);
+                if(u == v) return b - a;
+                else return u - v;
+            })
+            .mapToInt(a->a)
+            .toArray();
     }
 }
