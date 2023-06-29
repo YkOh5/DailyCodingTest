c8529b2adc617717928fd3a9caa2e569fcb85238
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-29 11:22:07 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.83 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -13,3 +13,29 @@ class Solution {
         return 10000 * validRankList.get(0)[1] + 100 * validRankList.get(1)[1] + validRankList.get(2)[1];
     }
 }
+
+
+
+// HashMap을 이용한 풀이
+// import java.util.Arrays;
+// import java.util.HashMap;
+// class Solution {
+//     public int solution(int[] rank, boolean[] attendance) {
+//         HashMap<Integer, Integer> rankMap = new HashMap<>();
+//         for (int idx = 0; idx < rank.length; idx++) {
+//             if (attendance[idx]) rankMap.put(rank[idx], idx);
+//         }
+//         Arrays.sort(rank);
+//         int coefficient = 10000;
+//         int answer = 0;
+//         int cnt = 0;
+//         for (int idx = 0; idx < rank.length; idx++) {
+//             if (rankMap.containsKey(rank[idx])) {
+//                 answer += rankMap.get(rank[idx]) * coefficient;
+//                 coefficient /= 100;
+//             }
+//             if (cnt == 3) break;
+//         }
+//         return answer;
+//     }
+// }
