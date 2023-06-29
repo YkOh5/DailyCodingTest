a57c45d5df6fef4e105e467f2b38e28adbe5a8b4
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-30 07:21:44 +0900

    [level 0] Title: 특이한 정렬, Time: 0.78 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -6,19 +6,13 @@ class Solution {
         for (int i = 0; i < numlist.length; i++) {
             answer[i] = numlist[i];
         }
+        
         Arrays.sort(answer, (num1, num2) -> {
-    int diff1 = Math.abs(n - num1);
-    int diff2 = Math.abs(n - num2);
-
-    if (diff1 == diff2) {
-        // 절대값이 같을 때 크기가 더 큰 수가 앞에 오도록 비교
-        return num2 - num1;
-    } else {
-        // 절대값이 다를 때 기존의 비교 방식 사용
-        return diff1 - diff2;
-    }
-});
-
+            int diff1 = Math.abs(n - num1);
+            int diff2 = Math.abs(n - num2);
+            
+            return (diff1 != diff2)? diff1 - diff2 : num2 - num1;
+        });
 
         return answer;
     }
