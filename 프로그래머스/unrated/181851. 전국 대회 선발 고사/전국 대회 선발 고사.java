a675e391cddbb9a28a0aed235d32e23a3b64624c
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-06-29 11:27:28 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.54 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -16,6 +16,24 @@ class Solution {
 
 
 
+// Stream을 이용한 풀이
+// import java.util.Comparator;
+// import java.util.stream.IntStream;
+
+// class Solution {
+//     public int solution(int[] rank, boolean[] attendance) {
+//         return IntStream.range(0, rank.length)
+//                 .filter(i -> attendance[i])
+//                 .boxed()
+//                 .sorted(Comparator.comparing(i -> rank[i]))
+//                 .limit(3L)
+//                 .reduce((current, next) -> current * 100 + next)
+//                 .get();
+//     }
+// }
+
+
+
 // HashMap을 이용한 풀이
 // import java.util.Arrays;
 // import java.util.HashMap;
