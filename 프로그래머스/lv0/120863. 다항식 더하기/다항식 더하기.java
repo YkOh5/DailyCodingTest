12ba102fe0159f0517b5d8ddd0287c4a061792f2
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-02 19:52:01 +0900

    [level 0] Title: 다항식 더하기, Time: 0.10 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -4,15 +4,19 @@ class Solution {
     private int coefA = 0, coefB = 0;
     
     public String solution(String polynomial) {
-        StringTokenizer st = new StringTokenizer(polynomial, " + ");
+        getCoefficients(polynomial);
+        
+        return buildAnswer(coefA, coefB);
+    }
+    
+    private void getCoefficients(String polynomial) {
+        StringTokenizer st = new StringTokenizer(polynomial, " + ");        
         while (st.hasMoreTokens()) {
             String term = st.nextToken();
             if (term.equals("x")) coefA++;
             else if (term.contains("x")) coefA += Integer.parseInt(term.replace("x", ""));
             else coefB += Integer.parseInt(term);
         }
-        
-        return buildAnswer(coefA, coefB);
     }
     
     private String buildAnswer(int coefA, int coefB) {
