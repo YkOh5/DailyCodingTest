d24e07a95b38a85ec9c964259d685b8afb55f378
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 08:47:20 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -2,16 +2,11 @@ class Solution {
     public String solution(String code) {
         StringBuilder sb = new StringBuilder();
         int mode = 0;
-        char[] chars = code.toCharArray();
-        
-        for (int idx = 0; idx < chars.length; idx++) {
-            if (chars[idx] == '1') {
-                mode = 1 - mode; // mode 값을 토글합니다.
-            } else if ((mode == 0 && idx % 2 == 0) || (mode == 1 && idx % 2 != 0)) {
-                sb.append(chars[idx]);
-            }
+        for (int idx = 0; idx < code.length(); idx++) {
+            if (code.charAt(idx) == '1') mode = 1 - mode;
+            else if ((mode == 0 && idx % 2 == 0) || (mode == 1 && idx % 2 != 0)) sb.append(code.charAt(idx));
         }
         
-        return (sb.length() == 0) ? "EMPTY" : sb.toString();
+        return (sb.length() == 0)? "EMPTY" : sb.toString();
     }
 }
