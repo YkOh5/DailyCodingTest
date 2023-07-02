71f6c39f99cde079880fe6fddae10320f5b35786
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 07:16:13 +0900

    [unrated] Title: 배열 만들기 2, Time: 3.66 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,36 +1,15 @@
-import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 class Solution {
-    public Integer[] solution(int l, int r) {
-        ArrayList<Integer> validNumList = new ArrayList<>();
-        for (int number = l; number <= r; number++) {
-            if (isValidNumber(number)) validNumList.add(number);
-        }
-        
-        return (validNumList.isEmpty())? new Integer[]{-1} : validNumList.toArray(new Integer[0]);
-    }
+    public int[] solution(int l, int r) {
+        int[] answer = IntStream.rangeClosed(l, r).filter(i -> {
+            while (i > 0) {
+                if (i % 5 != 0) return false;
+                i /= 10;
+            }
+            return true;
+        }).toArray();
 
-    private boolean isValidNumber(int number) {
-        for (char c : String.valueOf(number).toCharArray()) {
-            int targetNum = Character.getNumericValue(c);
-            if (targetNum != 0 && targetNum != 5) return false;
-        }
-
-        return true;
+        return answer.length == 0 ? new int[]{-1} : answer;
     }
 }
-
-
-
-// import java.util.ArrayList;
-
-// class Solution {
-//     public Integer[] solution(int l, int r) {
-//         ArrayList<Integer> validNumList = new ArrayList<>();              
-//         for (int i = l; i <= r; i++) {
-//             if (String.valueOf(i).replaceAll("[05]", "").length() == 0) validNumList.add(i);
-//         }
-
-//         return (validNumList.size() == 0)? new Integer[]{-1} : validNumList.toArray(new Integer[0]);
-//     }
-// }
