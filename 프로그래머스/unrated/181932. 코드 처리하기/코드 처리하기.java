b7c06fab64f0214bfdf5debbac80a53162d231f9
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 08:50:41 +0900

    [unrated] Title: 코드 처리하기, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -4,7 +4,8 @@ class Solution {
         int mode = 0;
         for (int idx = 0; idx < code.length(); idx++) {
             if (code.charAt(idx) == '1') mode = 1 - mode;
-            else if (idx % 2 == mode) sb.append(code.charAt(idx));
+            else if ((mode == 0 && idx % 2 == 0) || (mode == 1 && idx % 2 != 0)) sb.append(code.charAt(idx));
+            // else if (idx % 2 == mode) sb.append(code.charAt(idx));
         }
         
         return (sb.length() == 0)? "EMPTY" : sb.toString();
