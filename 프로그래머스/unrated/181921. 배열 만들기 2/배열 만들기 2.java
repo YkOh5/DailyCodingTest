26281318fadd6060c4fcfef2c7e2fa1e1c5bc1f5
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 04:59:47 +0900

    [unrated] Title: 배열 만들기 2, Time: 8.79 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -3,14 +3,10 @@ import java.util.ArrayList;
 class Solution {
     public Integer[] solution(int l, int r) {
         ArrayList<Integer> validNumList = new ArrayList<>();
-        // for (int number = l; number <= r; number++) {
-        //     if (isValidNumber(number)) validNumList.add(number);
-        // }
-        
-        for (int i = l; i <= r; i++) {
-            if (String.valueOf(i).replaceAll("[05]", "").length() == 0) validNumList.add(i);
+        for (int number = l; number <= r; number++) {
+            if (isValidNumber(number)) validNumList.add(number);
         }
-
+        
         return (validNumList.size() == 0)? new Integer[]{-1} : validNumList.toArray(new Integer[0]);
     }
 
