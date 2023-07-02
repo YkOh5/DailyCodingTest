2e2eeceee78a554dd62b2212aca86f915a13c88e
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 03:52:53 +0900

    [unrated] Title: 배열 만들기 2, Time: 13.88 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,21 +1,25 @@
-import java.util.stream.IntStream;
+import java.util.*;
 
 class Solution {
+    public int[] deletes = {1, 2, 3, 4, 6, 7, 8, 9};
     public int[] solution(int l, int r) {
-        int[] answer = IntStream.rangeClosed(l,r).filter(this::valid).toArray();
-        return answer.length==0?new int[]{-1}:answer;
-    }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = l;i <= r;i++) {
+            boolean isDelete = false;
+            String value = Integer.toString(i);
+            for (int delete : deletes)
+                if (value.contains(Integer.toString(delete))) {
+                    isDelete = true;
+                    break;
+                }
 
-    private boolean valid(int i) {
-        boolean result = true;
-
-        for(char ch: String.valueOf(i).toCharArray()) {
-            if(ch != '0' && ch != '5') {
-                result = false;
-                break;
-            }
-        }
-
-        return result;
+            if (isDelete) continue;
+            arrayList.add(i);
+        }       
+        int idx = 0;
+        int[] answer = new int[arrayList.size()];
+        for (int data : arrayList)
+            answer[idx++] = data;
+        return arrayList.size() == 0 ? new int[] {-1} : answer;
     }
 }
