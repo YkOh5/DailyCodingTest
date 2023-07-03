201c9f9f41f1cf992969e20f816376985e808324
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 11:09:19 +0900

    [level 0] Title: OX퀴즈, Time: 0.14 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -1,35 +1,35 @@
-// class Solution {
-//     public String[] solution(String[] quiz) {
-//         int idx = 0;
-//         for (String equation : quiz) {
-//             int sum = 0;
-//             boolean positiveSign = true;
-//             String[] eqArr = equation.split(" ");            
-//             for (String str : eqArr) {
-//                 if (str.equals("+")) positiveSign = true;
-//                 else if (str.equals("-")) positiveSign = false;
-//                 else if (str.equals("="))
-//                     quiz[idx] = (sum == Integer.parseInt(eqArr[eqArr.length - 1]))? "O" : "X";
-//                 else {
-//                     sum += (positiveSign)? Integer.parseInt(str) : Integer.parseInt(str) * -1;
-//                 }
-//             }            
-//             idx++;
-//         }
-
-//         return quiz;
-//     }
-// }
-
-
 class Solution {
-    public String[] solution(String[] quiz) {        
-        for (int idx = 0; idx < quiz.length; idx++) {
-            String[] str = quiz[idx].split(" ");
-            int sum = Integer.parseInt(str[0]) + Integer.parseInt(str[2]) * ((str[1].equals("+")) ? 1 : -1);
-            quiz[idx] = (Integer.parseInt(str[4]) == sum)? "O" : "X";            
+    public String[] solution(String[] quiz) {
+        int idx = 0;
+        for (String equation : quiz) {
+            int sum = 0;
+            boolean positiveSign = true;
+            String[] eqArr = equation.split(" ");            
+            for (String str : eqArr) {
+                if (str.equals("+")) positiveSign = true;
+                else if (str.equals("-")) positiveSign = false;
+                else if (str.equals("="))
+                    quiz[idx] = (sum == Integer.parseInt(eqArr[eqArr.length - 1]))? "O" : "X";
+                else {
+                    sum += (positiveSign)? Integer.parseInt(str) : Integer.parseInt(str) * -1;
+                }
+            }            
+            idx++;
         }
-        
+
         return quiz;
     }
 }
+
+
+// class Solution {
+//     public String[] solution(String[] quiz) {        
+//         for (int idx = 0; idx < quiz.length; idx++) {
+//             String[] str = quiz[idx].split(" ");
+//             int sum = Integer.parseInt(str[0]) + Integer.parseInt(str[2]) * ((str[1].equals("+")) ? 1 : -1);
+//             quiz[idx] = (Integer.parseInt(str[4]) == sum)? "O" : "X";            
+//         }
+        
+//         return quiz;
+//     }
+// }
