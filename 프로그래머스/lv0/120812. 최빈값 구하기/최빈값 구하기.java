55b49feca4823536797d6840de6dbfba7c75c933
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 09:38:45 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.11 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,27 +1,49 @@
 import java.util.HashMap;
 
 class Solution {
-    private HashMap<Integer, Integer> cntMap = new HashMap<>();
-
     public int solution(int[] array) {
+        HashMap<Integer, Integer> cntMap = new HashMap<>();
+        int maxCnt = 0, domNum = 1;
         for (int number : array) {
             cntMap.put(number, cntMap.getOrDefault(number, 0) + 1);
-        }
-
-        return getDominantNumber();
-    }
-
-    private int getDominantNumber() {
-        int maxCnt = 0;
-        int domNum = -1;
-        for (int key : cntMap.keySet()) {
-            int cnt = cntMap.get(key);
+            
+            int cnt = cntMap.get(number);
             if (cnt > maxCnt) {
-                domNum = key;
+                domNum = number;
                 maxCnt = cnt;
             } else if (cnt == maxCnt) domNum = -1;
         }
-
+        
         return domNum;
     }
 }
+
+
+
+// import java.util.HashMap;
+
+// class Solution {
+//     private HashMap<Integer, Integer> cntMap = new HashMap<>();
+
+//     public int solution(int[] array) {
+//         for (int number : array) {
+//             cntMap.put(number, cntMap.getOrDefault(number, 0) + 1);
+//         }
+
+//         return getDominantNumber();
+//     }
+
+//     private int getDominantNumber() {
+//         int maxCnt = 0;
+//         int domNum = -1;
+//         for (int key : cntMap.keySet()) {
+//             int cnt = cntMap.get(key);
+//             if (cnt > maxCnt) {
+//                 domNum = key;
+//                 maxCnt = cnt;
+//             } else if (cnt == maxCnt) domNum = -1;
+//         }
+
+//         return domNum;
+//     }
+// }
