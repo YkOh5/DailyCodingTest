9a056ccf0f8cdaa511a8f85c1458f1735102a32f
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-03 09:36:39 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.17 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,19 +1,27 @@
 import java.util.HashMap;
 
 class Solution {
+    private HashMap<Integer, Integer> cntMap = new HashMap<>();
+
     public int solution(int[] array) {
-        HashMap<Integer, Integer> cntMap = new HashMap<>();
-        int maxCnt = 0, domNum = 1;
         for (int number : array) {
             cntMap.put(number, cntMap.getOrDefault(number, 0) + 1);
-            
-            int cnt = cntMap.get(number);
+        }
+
+        return getDominantNumber();
+    }
+
+    private int getDominantNumber() {
+        int maxCnt = 0;
+        int domNum = -1;
+        for (int key : cntMap.keySet()) {
+            int cnt = cntMap.get(key);
             if (cnt > maxCnt) {
-                domNum = number;
+                domNum = key;
                 maxCnt = cnt;
             } else if (cnt == maxCnt) domNum = -1;
         }
-        
+
         return domNum;
     }
 }
