92f81f479da2cdbe68952a9ee93d5937c8d8c13b
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-07-15 11:43:09 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.07 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,26 +1,53 @@
-// 아래와 같은 방식보다는 변위를 사용한 코드가 유지보수 측면에서 더 좋을 수 있다
+// class Solution {
+//     public int[][] solution(int n) {
+//         int[][] answer = new int[n][n];
+//         int num = 1;
+//         int row = 0, column = 0;
+//         int mode = 0, loopCnt = n;
 
+//         while (num <= n * n) {
+//             for (int i = 0; i < loopCnt; i++) {
+//                 answer[row][column] = num++;                
+//                 if (i == loopCnt - 1) mode = (mode + 1) % 4;                
+//                 switch (mode) {
+//                     case 0 -> column++;
+//                     case 1 -> row++;
+//                     case 2 -> column--;
+//                     case 3 -> row--;
+//                 }
+//             }
+//             if (mode % 2 != 0) loopCnt--;
+//         }
+
+//         return answer;
+//     }
+// }
+
+
+
+// 변위를 사용한 코드가 유지보수 측면에서 더 좋을 수 있다
 class Solution {
     public int[][] solution(int n) {
+        int[][] distance = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         int[][] answer = new int[n][n];
-        int num = 1;
-        int row = 0, column = 0;
-        int mode = 0, loopCnt = n;
-
+        int row = 0, column = 0;        
+        int num = 1, idx = 0;
+        
         while (num <= n * n) {
-            for (int i = 0; i < loopCnt; i++) {
-                answer[row][column] = num++;                
-                if (i == loopCnt - 1) mode = (mode + 1) % 4;                
-                switch (mode) {
-                    case 0 -> column++;
-                    case 1 -> row++;
-                    case 2 -> column--;
-                    case 3 -> row--;
-                }
+            answer[row][column] = num++;
+            int nRow = row + distance[idx][0];
+            int nColumn = column + distance[idx][1];
+            
+            if (nRow < 0 || nRow >= n || nColumn < 0 || nColumn >= n || answer[nRow][nColumn] != 0) {
+                idx = (idx + 1) % 4;
+                nRow = row + distance[idx][0];
+                nColumn = column + distance[idx][1];
             }
-            if (mode % 2 != 0) loopCnt--;
+            
+            row = nRow;
+            column = nColumn;
         }
-
+        
         return answer;
     }
 }
