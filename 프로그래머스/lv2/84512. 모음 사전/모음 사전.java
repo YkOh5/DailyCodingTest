31c63687943ea54827af7b0fd9fa3d643dbd0496
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2023-08-10 18:57:13 +0900

    [level 2] Title: 모음 사전, Time: 4.55 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,28 +1,27 @@
 import java.util.*;
 
 class Solution {
-    private String word;
-    private int answer, maxLength;
-    private int cnt = 0;    
+    private String[] vowels = {"A", "E", "I", "O", "U"};
+    private String word;    
+    private int maxLength = vowels.length;    
+    private int cnt = 0;
+    private int answer;
     
     
     public int solution(String word) {
-        String[] vowels = {"A", "E", "I", "O", "U"};
-        this.word = word;
-        maxLength = vowels.length;
-
-        getValidCnt(vowels, new String());
+        this.word = word;        
+        getValidCnt(new StringBuilder());
         
         return answer;
     }
 
-    private void getValidCnt(String[] vowels, String curStr) {
+    private void getValidCnt(StringBuilder curStr) {
         if (curStr.length() <= maxLength) {
-            if (word.equals(curStr)) answer = cnt;
+            if (word.equals(curStr.toString())) answer = cnt;
             cnt++;
             
             for (String vowel : vowels) {
-                getValidCnt(vowels, curStr + vowel);
+                getValidCnt(new StringBuilder(curStr).append(vowel));
             }
         }
     }
