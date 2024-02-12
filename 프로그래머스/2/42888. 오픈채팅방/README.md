2e7c8e9ec005bde54a073e476ef82022906a7407
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-02-13 00:26:52 +0900

    [level 2] Title: 오픈채팅방, Time: 265.27 ms, Memory: 228 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888#) 
 
 ### 성능 요약
 
-메모리: 229 MB, 시간: 201.90 ms
+메모리: 228 MB, 시간: 265.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 1일 21:29:56
+2024년 2월 2일 0:26:49
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,28 +3,28 @@ import java.util.*;
 class Solution {    
     private HashMap<String, String> uidNickMap = new HashMap<>();
     private ArrayList<String> userAccessLogs = new ArrayList<>();
-    
+
     public String[] solution(String[] record) {
         setNickName(record);
         updateAccessLogs(record);
-        
+
         return userAccessLogs.toArray(new String[0]);
     }
-    
+
     private void setNickName(String[] record) {
         for (int i = 0; i < record.length; i++) {
             String[] partialRecord = record[i].split("\\s+");
-            
+
             if (partialRecord[0].equals("Enter") || partialRecord[0].equals("Change")) {
                 uidNickMap.put(partialRecord[1], partialRecord[2]);
             }
         }
     }
-    
+
     private void updateAccessLogs(String[] record) {
         for (int i = 0; i < record.length; i++) {
             String[] partialRecord = record[i].split("\\s+");
-            
+
             if (partialRecord[0].equals("Enter")) {
                 userAccessLogs.add(uidNickMap.get(partialRecord[1]) + "님이 들어왔습니다.");
             } else if (partialRecord[0].equals("Leave")) {
@@ -33,3 +33,42 @@ class Solution {
         }
     }
 }
+
+// Change 로그를 제외한 Enter와 Leave 로그만 추출한 자료를 활용하고
+// String.split() 대신 StringTokenizer.nextToken()을 사용하면 처리속도를 높일 수 있다
+
+// class Solution {
+//     private HashMap<String, String> nicknameMap = new HashMap<>();
+//     private ArrayList<String[]> tempAccessLogs = new ArrayList<>();
+//     private ArrayList<String> userAccessLogs = new ArrayList<>();
+    
+//     public String[] solution(String[] record) {
+//         processRawData(record);
+//         updateAccessLogs(record);
+        
+//         return userAccessLogs.toArray(new String[0]);
+//     }
+    
+//     private void processRawData(String[] record) {
+//         for (int i = 0; i < record.length; i++) {
+//             StringTokenizer st = new StringTokenizer(record[i], " ");
+//             String cmd = st.nextToken();
+//             String uid = st.nextToken();
+            
+//             if (cmd.equals("Enter")) {
+//                 tempAccessLogs.add(new String[]{uid, "님이 들어왔습니다."});
+//                 nicknameMap.put(uid, st.nextToken());
+//             } else if (cmd.equals("Leave")) {
+//                 tempAccessLogs.add(new String[]{uid, "님이 나갔습니다."});
+//             } else if (cmd.equals("Change")) {
+//                 nicknameMap.put(uid, st.nextToken());
+//             }
+//         }
+//     }
+    
+//     private void updateAccessLogs(String[] record) {
+//         for (int i = 0; i < tempAccessLogs.size(); i++) {
+//             userAccessLogs.add(nicknameMap.get(tempAccessLogs.get(i)[0]) + tempAccessLogs.get(i)[1]);
+//         }
+//     }    
+// }
