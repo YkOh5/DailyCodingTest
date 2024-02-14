2e1dd82fb61d30febe97f9381dbdb8effe0b7a5b
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-02-15 01:33:35 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.16 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    private final int[] discountRates = {10, 20, 30, 40};
+    private int[][] users;
+    private int[] emoticons;
+    private int[] goalPoint = new int[]{0, 0};    
+    
+    public int[] solution(int[][] users, int[] emoticons) {
+        this.users = users;
+        this.emoticons = emoticons;
+        
+        Stack<Integer> stack = new Stack<>();
+        searchGoalPoint(stack);
+        
+        return goalPoint;
+    }
+    
+    private void searchGoalPoint(Stack<Integer> stack) {
+        if (stack.size() >= emoticons.length) {
+            updateGoalPoint(stack);
+        } else {
+            for (int i = 0; i < discountRates.length; i++) {
+                stack.push(discountRates[i]);
+                searchGoalPoint(stack);
+                if (!stack.isEmpty()) stack.pop();
+            }
+        }
+    }
+    
+    private void updateGoalPoint(Stack<Integer> stack) {
+        int[] dcrArr = new int[emoticons.length];
+        int subscriberCnt = 0;
+        int totalSalesRevenue = 0;
+        
+        for (int i = 0; i < emoticons.length; i++) {
+            dcrArr[i] = stack.elementAt(i);
+        }
+        
+        for (int i = 0; i < users.length; i++) {
+            int personalSpending = 0;
+            
+            for (int j = 0; j < emoticons.length; j++) {
+                if (dcrArr[j] >= users[i][0]) {
+                    personalSpending += emoticons[j] * (100 - dcrArr[j]) / 100;
+                }
+            }
+            
+            if (personalSpending >= users[i][1]) subscriberCnt++;
+            else totalSalesRevenue += personalSpending;
+        }
+        
+        if (subscriberCnt > goalPoint[0]) {
+            goalPoint[0] = subscriberCnt;
+            goalPoint[1] = totalSalesRevenue;
+        } else if (subscriberCnt == goalPoint[0] && totalSalesRevenue > goalPoint[1]) {
+            goalPoint[1] = totalSalesRevenue;
+        }
+    }
+}
