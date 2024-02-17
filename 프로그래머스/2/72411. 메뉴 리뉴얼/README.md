594389576301baba5f95b2de09959310c023c580
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-02-17 22:51:04 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5.61 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 4.04 ms
+메모리: 81.5 MB, 시간: 5.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 22:25:28
+2024년 2월 6일 22:51:3
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 class Solution {
     private HashMap<String, Integer> commonMenuMap = new HashMap<>();
-    
+
     public String[] solution(String[] orders, int[] course) {
         getCommonMenuMap(orders, course);
-        
+
         return getCourseMenuOptions();
     }
-    
+
     private void getCommonMenuMap(String[] orders, int[] course) {
         HashSet<Integer> dishCntSet = new HashSet<>();
         for (int dishCnt : course) dishCntSet.add(dishCnt); 
-        
+
         for (String order : orders) {
             if (order.length() >= course[0]) {
                 StringBuilder orderSubset = new StringBuilder();
@@ -22,37 +22,37 @@ class Solution {
             }
         }
     }
-    
+
     private void getValidOrderSubsets(String sortedOrder, HashSet<Integer> dishCntSet, StringBuilder orderSubset, int index) {
         if (dishCntSet.contains(orderSubset.length())) {
             commonMenuMap.put(orderSubset.toString(), commonMenuMap.getOrDefault(orderSubset.toString(), 0) + 1);
         }
-        
+
         for (int i = index; i < sortedOrder.length(); i++) {
             orderSubset.append(sortedOrder.charAt(i));
             getValidOrderSubsets(sortedOrder, dishCntSet, orderSubset, i + 1);
             orderSubset.deleteCharAt(orderSubset.length() - 1);
         }
     }
-    
+
     private String[] getCourseMenuOptions() {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> courseMenuOptionslist = new ArrayList<>();
         HashMap<Integer, Integer> orderCntMap = new HashMap<>();
-        
+
         for (String key : commonMenuMap.keySet()) {            
             if (commonMenuMap.get(key) > orderCntMap.getOrDefault(key.length(), 0)) {
                 orderCntMap.put(key.length(), commonMenuMap.get(key));
             }
         }
-        
+
         for (String key : commonMenuMap.keySet()) {
             if (commonMenuMap.get(key) >= 2 && commonMenuMap.get(key) == orderCntMap.get(key.length())) {
-                list.add(key);
+                courseMenuOptionslist.add(key);
             }
         }
-        
-        Collections.sort(list);
-        
-        return list.toArray(new String[0]);
+
+        Collections.sort(courseMenuOptionslist);
+
+        return courseMenuOptionslist.toArray(new String[0]);
     }
 }
