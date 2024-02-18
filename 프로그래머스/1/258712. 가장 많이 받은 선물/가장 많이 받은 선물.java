57b35f92908aa301d271b6b722593ffdc9964ed7
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-02-18 17:18:14 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 8.10 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,30 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        Map<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> giverTakerMap= new HashMap<>();
+        HashMap<String, Integer> giftIndexMap = new HashMap<>();
+
+        for (String gift : gifts) {
+            StringTokenizer st = new StringTokenizer(gift, " ");
+            String giver = st.nextToken();
+            String taker = st.nextToken();
+
+            giverTakerMap.put(gift, giverTakerMap.getOrDefault(gift, 0) + 1);
+            giftIndexMap.put(giver, giftIndexMap.getOrDefault(giver, 0) + 1);
+            giftIndexMap.put(taker, giftIndexMap.getOrDefault(taker, 0) - 1);
+        }
+
+        int maxNextMonthGiftCnt = Integer.MIN_VALUE;
+
         for (int i = 0; i < friends.length; i++) {
-            map.put(friends[i], i);
-        }
-        int[] index = new int[friends.length];
-        int[][] record = new int[friends.length][friends.length];
+            int nextMonthGiftCnt = 0;
 
-        for (String str : gifts) {
-            String[] cur = str.split(" ");
-            index[map.get(cur[0])]++;
-            index[map.get(cur[1])]--;
-            record[map.get(cur[0])][map.get(cur[1])]++;
+            for (int j = 0; j < friends.length; j++) {
+                if (i == j) continue;
+
+                int giveCnt = giverTakerMap.getOrDefault(new StringBuilder().append(friends[i]).append(" ").append(friends[j]).toString(), 0);
+                int takeCnt = giverTakerMap.getOrDefault(new StringBuilder().append(friends[j]).append(" ").append(friends[i]).toString(), 0);
+
+                if (giveCnt > takeCnt) nextMonthGiftCnt++;
+                else if (giveCnt == takeCnt && giftIndexMap.getOrDefault(friends[i], 0) > giftIndexMap.getOrDefault(friends[j], 0)) nextMonthGiftCnt++;
+            }
+
+            maxNextMonthGiftCnt = Math.max(maxNextMonthGiftCnt, nextMonthGiftCnt);
         }
 
-       int ans = 0;
-       for (int i = 0; i < friends.length; i++) {
-           int cnt = 0;
-           for (int j = 0; j < friends.length; j++) {
-               if(i == j) continue;
-               if (record[i][j] > record[j][i]) cnt++;
-               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
-           }
-           ans = Math.max(cnt, ans);
-       }
-        return ans;
+        return maxNextMonthGiftCnt;
     }
 }
