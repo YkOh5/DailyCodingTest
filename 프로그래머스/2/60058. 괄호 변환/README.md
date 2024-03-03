8289c91887a73aafe08a0e4c8b92c8c431a504ab
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-03 10:49:42 +0900

    [level 2] Title: 괄호 변환, Time: 1.11 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/README.md
+++ 프로그래머스/2/60058. 괄호 변환/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 변환 - 60058 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058#) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 1.10 ms
+메모리: 78.1 MB, 시간: 1.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 03일 10:33:11
+2024년 03월 03일 10:49:41
 
 ### 문제 설명
 
--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -10,28 +10,27 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         StringBuilder u = new StringBuilder();
         Stack<Character> stack = new Stack<>();
-        int cnt1 = 0;
-        int cnt2 = 0;
+        int cnt = 0;
         
         for (int i = 0; i < parentheses.length(); i++) {
             char parenthesis = parentheses.charAt(i);
             u.append(parenthesis);
             
             if (parenthesis == '(') {
-                cnt1++;
+                cnt++;
                 stack.push(parenthesis);
             } else {
-                cnt2++;
+                cnt--;
                 if (!stack.isEmpty() && stack.peek() == '(') stack.pop();
                 else stack.push(parenthesis);
             }
             
-            if (cnt1 == cnt2 && stack.isEmpty()) {
+            if (cnt == 0 && stack.isEmpty()) {
                 sb.append(u);
                 if (i < parentheses.length() - 1)
                     sb.append(getValidParentheses(parentheses.substring(i + 1)));
                 break;
-            } else if (cnt1 == cnt2 && !stack.isEmpty()) {
+            } else if (cnt == 0 && !stack.isEmpty()) {
                 sb.append('(');
                 if (i < parentheses.length() - 1)
                     sb.append(getValidParentheses(parentheses.substring(i + 1)));
