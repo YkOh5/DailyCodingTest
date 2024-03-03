6f186e5b30b1d0ea5d0b0d8bc4b2463eb026ae96
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-03 10:33:13 +0900

    [level 2] Title: 괄호 변환, Time: 1.10 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String parentheses) {
+        
+        return getValidParentheses(parentheses);
+    }
+    
+    private String getValidParentheses(String parentheses) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder u = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        
+        for (int i = 0; i < parentheses.length(); i++) {
+            char parenthesis = parentheses.charAt(i);
+            u.append(parenthesis);
+            
+            if (parenthesis == '(') {
+                cnt1++;
+                stack.push(parenthesis);
+            } else {
+                cnt2++;
+                if (!stack.isEmpty() && stack.peek() == '(') stack.pop();
+                else stack.push(parenthesis);
+            }
+            
+            if (cnt1 == cnt2 && stack.isEmpty()) {
+                sb.append(u);
+                if (i < parentheses.length() - 1)
+                    sb.append(getValidParentheses(parentheses.substring(i + 1)));
+                break;
+            } else if (cnt1 == cnt2 && !stack.isEmpty()) {
+                sb.append('(');
+                if (i < parentheses.length() - 1)
+                    sb.append(getValidParentheses(parentheses.substring(i + 1)));
+                sb.append(')');
+                for (int j = 1; j < u.length() - 1; j++) {
+                    if (u.charAt(j) == '(') sb.append(')');
+                    else sb.append('(');
+                }                
+                break;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
