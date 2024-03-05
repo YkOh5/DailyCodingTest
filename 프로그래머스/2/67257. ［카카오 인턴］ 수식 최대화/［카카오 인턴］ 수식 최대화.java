e34bd307d098ea0b64fa923a4b7d9550b9fe8e96
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-05 12:54:47 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.57 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+class Solution {
+    public long solution(String expression) {
+        // 연산자 우선순위
+        String[] opPriority = {"+-*", "+*-", "-+*", "-*+", "*+-", "*-+"};
+        ArrayList<Character> opList = new ArrayList<>();
+        ArrayList<Long> numList = new ArrayList<>();
+        StringBuilder num = new StringBuilder();
+        long maxReward= 0;
+
+        // 숫자와 연산자를 각각의 리스트에 저장
+        for (char c : expression.toCharArray()) {
+            if (Character.isDigit(c)) {
+                num.append(c);
+            } else {
+                opList.add(c);
+                numList.add(Long.parseLong(num.toString()));
+                num.setLength(0);
+            }
+        }
+        // 마지막 숫자 추가
+        numList.add(Long.parseLong(num.toString()));
+
+        for (String ops : opPriority) {
+            ArrayList<Long> tempNumList = new ArrayList<>(numList);
+            ArrayList<Character> tempOpList = new ArrayList<>(opList);
+            
+            for (char op : ops.toCharArray()) {
+                int idx = 0;
+                // 리스트의 범위 내에서 반복
+                while (idx < tempOpList.size()) {
+                    // 리스트의 연산자가 우선순위에 부합하면
+                    if (tempOpList.get(idx) == op) {
+                        // 앞의 숫자를 연산결과로 갱신
+                        tempNumList.set(idx, operate(tempNumList.get(idx), op, tempNumList.get(idx + 1)));
+                        // 뒤의 숫자를 리스트에서 제거
+                        tempNumList.remove(idx + 1);
+                        // 해당 연산자를 리스트에서 제거
+                        tempOpList.remove(idx);
+                    // 우선순위에 부합하지 않으면
+                    } else {
+                        // 다음 차례로
+                        idx++;
+                    }
+                }
+            }
+            // 최대상금 갱신
+            maxReward = Math.max(maxReward, Math.abs(tempNumList.get(0)));
+        }
+        
+        return maxReward;
+    }
+
+    private long operate(long num1, char op, long num2) {
+        if (op == '+') return num1 + num2;
+        else if (op == '-') return num1 - num2;
+        
+        return num1 * num2;
+    }
+}
