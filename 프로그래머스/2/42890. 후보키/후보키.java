fe0beb251c1208933d80270c46421bc94a26e9f0
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-15 03:12:14 +0900

    [level 2] Title: 후보키, Time: 1.76 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,20 +1,21 @@
 import java.util.*;
 
 class Solution {
+    private ArrayList<ArrayList<Integer>> subsetList = new ArrayList<>();
+    
     public int solution(String[][] relation) {
-        int[] arr = new int[relation[0].length];
-        ArrayList<ArrayList<Integer>> subsetList = new ArrayList<>();
-        
+        int[] arr = new int[relation[0].length];        
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
         }
         
         for (int limit = 1; limit <= arr.length; limit++) {
-            getSubsets(arr, new ArrayList<>(), 0, limit, subsetList);
+            getSubsets(arr, new ArrayList<>(), 0, limit);
         }
         
         ArrayList<ArrayList<Integer>> cKeyList = new ArrayList<>();
         for (ArrayList<Integer> subset : subsetList) {
+            // 최소성 검증
             boolean minimal = true;
             for (ArrayList<Integer> cKey : cKeyList) {
                 if (subset.containsAll(cKey)) {
@@ -25,8 +26,8 @@ class Solution {
             
             if (!minimal) continue;
             
-            HashSet<ArrayList<String>> subsetSet = new HashSet<>();
-            
+            // 유일성 검증
+            HashSet<ArrayList<String>> subsetSet = new HashSet<>();            
             for (int i = 0; i < relation.length; i++) {
                 ArrayList<String> list = new ArrayList<>();
                 for (int num : subset) {
@@ -39,13 +40,11 @@ class Solution {
             
             if (subsetSet.size() == relation.length) cKeyList.add(subset);
         }
-        
-        System.out.println(cKeyList);
                 
         return cKeyList.size();
     }
     
-    private void getSubsets(int[] arr, ArrayList<Integer> subset, int idx, int limit, ArrayList<ArrayList<Integer>> subsetList) {
+    private void getSubsets(int[] arr, ArrayList<Integer> subset, int idx, int limit) {
         if (subset.size() == limit) {
             subsetList.add(new ArrayList<>(subset));
             
@@ -54,7 +53,7 @@ class Solution {
         
         for (int i = idx; i < arr.length; i++) {
             subset.add(arr[i]);
-            getSubsets(arr, subset, i + 1, limit, subsetList);
+            getSubsets(arr, subset, i + 1, limit);
             subset.remove(subset.size() - 1);
         }
     }
