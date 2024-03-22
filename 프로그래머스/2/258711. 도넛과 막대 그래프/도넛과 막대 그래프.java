9db2df6cfa890da76e3b51121ffc2c3d673b046a
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-22 21:34:14 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.09 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[][] edges) {
-        HashMap<Integer, Integer> cntMap1 = new HashMap<>();
-        HashMap<Integer, Integer> cntMap2 = new HashMap<>();
+        HashMap<Integer, Integer> outCntMap = new HashMap<>();
+        HashMap<Integer, Integer> inCntMap = new HashMap<>();
         
         for (int[] edge : edges) {
-            cntMap1.put(edge[0], cntMap1.getOrDefault(edge[0], 0) + 1);
-            cntMap2.put(edge[1], cntMap2.getOrDefault(edge[1], 0) + 1);
+            outCntMap.put(edge[0], outCntMap.getOrDefault(edge[0], 0) + 1);
+            inCntMap.put(edge[1], inCntMap.getOrDefault(edge[1], 0) + 1);
         }
         
         int[] answer = new int[4];
         
-        for (int key : cntMap1.keySet()) {
-            if (cntMap1.get(key) >= 2 && !cntMap2.containsKey(key)) answer[0] = key;
-            if (cntMap1.get(key) == 2 && cntMap2.getOrDefault(key, 0) >= 2) answer[3]++;
+        for (int key : outCntMap.keySet()) {
+            if (outCntMap.get(key) >= 2 && !inCntMap.containsKey(key)) answer[0] = key;
+            if (outCntMap.get(key) == 2 && inCntMap.getOrDefault(key, 0) >= 2) answer[3]++;
         }
         
-        for (int key : cntMap2.keySet()) {
-            if (cntMap2.get(key) > 0 && !cntMap1.containsKey(key)) answer[2]++;
+        for (int key : inCntMap.keySet()) {
+            if (inCntMap.get(key) > 0 && !outCntMap.containsKey(key)) answer[2]++;
         }
         
-        answer[1] = cntMap1.get(answer[0]) - answer[2] - answer[3];
+        answer[1] = outCntMap.get(answer[0]) - answer[2] - answer[3];
         
         return answer;
     }
