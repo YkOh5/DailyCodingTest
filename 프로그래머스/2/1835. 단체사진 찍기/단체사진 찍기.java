ec792961f74b7cb2b45519d210697cc9212530ff
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-23 13:51:12 +0900

    [level 2] Title: 단체사진 찍기, Time: 1624.29 ms, Memory: 394 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -34,7 +34,7 @@ class Solution {
     }
     
     private boolean isValidPerm(char[] perm) {
-        HashMap<Character, Integer> posMap = new HashMap<>();        
+        HashMap<Character, Integer> posMap = new HashMap<>();
         for (int i = 0; i < perm.length; i++) {
             posMap.put(perm[i], i);
         }
@@ -52,3 +52,24 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+// 유효한 순열인지 확인하는 함수에서 굳이 HashMap을 쓸 필요는 없었다
+// 아래와 같이 String.indexOf()를 활용하면 더 효율적인 코드를 작성할 수 있다
+
+// private boolean isValidPerm(char[] perm) {    
+//     String str = new String(perm);
+    
+//     for (String cond : data) {
+//         int d1 = Math.abs(str.indexOf(cond.charAt(0)) - str.indexOf(cond.charAt(2)));
+//         int d2 = Character.getNumericValue(cond.charAt(4)) + 1;
+//         char op = cond.charAt(3);
+            
+//         if ((op == '=' && d1 != d2) || (op == '<' && d1 >= d2) || (op == '>' && d1 <= d2))
+//             return false;
+//     }
+    
+//     return true;
+// }
