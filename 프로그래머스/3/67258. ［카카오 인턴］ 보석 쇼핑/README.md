232d527825315413f5c674a1957d938c185e1fa9
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-25 10:06:18 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 81.74 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 보석 쇼핑 - 67258 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258#) 
 
 ### 성능 요약
 
-메모리: 82.9 MB, 시간: 69.73 ms
+메모리: 82.6 MB, 시간: 81.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 06:06:56
+2024년 03월 25일 10:06:18
 
 ### 문제 설명
 
--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = new int[2];        
+        int[] answer = {0, Integer.MAX_VALUE};        
         HashSet<String> fullGemSet = new HashSet<>();
-        HashMap<String, Integer> gemMap = new HashMap<>();
         
         for (String gem : gems) {
             fullGemSet.add(gem);
         }
                 
+        HashMap<String, Integer> gemCntMap = new HashMap<>();
         HashSet<String> curGemSet = new HashSet<>();        
         int fullSetSize = fullGemSet.size();
-        int minRange = Integer.MAX_VALUE;
         int p1 = 0, p2 = -1;
         
         while(p1 < gems.length - fullSetSize || p2 < gems.length - 1) {
             if (curGemSet.size() >= fullSetSize || p2 == gems.length - 1) {
                 p1++;
-                gemMap.put(gems[p1 - 1], gemMap.getOrDefault(gems[p1 - 1], 0) - 1);
-                if (gemMap.get(gems[p1 - 1]) == 0) curGemSet.remove(gems[p1 - 1]);
+                gemCntMap.put(gems[p1 - 1], gemCntMap.getOrDefault(gems[p1 - 1], 0) - 1);
+                if (gemCntMap.get(gems[p1 - 1]) == 0) curGemSet.remove(gems[p1 - 1]);
             } else if (curGemSet.size() < fullSetSize) {
                 p2++;
-                gemMap.put(gems[p2], gemMap.getOrDefault(gems[p2], 0) + 1);
+                gemCntMap.put(gems[p2], gemCntMap.getOrDefault(gems[p2], 0) + 1);
                 curGemSet.add(gems[p2]);
             }
             
-            if (curGemSet.size() == fullSetSize && p2 - p1 < minRange) {
-                minRange = p2 - p1;
+            if (curGemSet.size() == fullSetSize && p2 - p1 < answer[1] - answer[0]) {
                 answer[0] = p1 + 1;
                 answer[1] = p2 + 1;
             }
