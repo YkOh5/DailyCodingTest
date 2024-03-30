8c62ef0015430e59121b93568cb2a0c060ce7989
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-03-30 10:35:16 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 5.11 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,56 +1,36 @@
 import java.util.*;
 
 class Solution {
-  public String solution(int n, int t, int m, String[] timetable) {
-        List<Integer> timetableToInt = new ArrayList<>(timetable.length); 
-        for (int i = 0; i < timetable.length; i++) {
-            int hours = parseInt(timetable[i].substring(0, 2));
-            int minutes = parseInt(timetable[i].substring(3, 5));
-            timetableToInt.add(hours * 60 + minutes);
+    public String solution(int n, int t, int m, String[] timetable) {
+        ArrayList<Integer> timeList = new ArrayList<>();
+        
+        for (String time : timetable) {
+            StringTokenizer st = new StringTokenizer(time, ":");
+            int hour = Integer.parseInt(st.nextToken());
+            int minute = Integer.parseInt(st.nextToken());
+            
+            timeList.add(hour * 60 + minute);
         }
-
-        int first = 540;
-        int last = first + (n - 1) * t;
-        int conPassTime = 0;
-
-        Collections.sort(timetableToInt);
-        Queue<Integer> crewQueue = new LinkedList<>(timetableToInt); 
+        
+        Collections.sort(timeList);
+                
+        int lastArvTime = 0;
+        boolean isLastShuttleFull = false;
+        
         for (int i = 0; i < n; i++) {
-            int validTime = first + i * t;
-            int onBoardCnt = 0;
-            int lastBoardTime = 0;
-            for (int j = 0; j < m; j++) {
-                if (crewQueue.isEmpty()) {
-                    break;
-                }
-                int targetCrewTime = ((LinkedList<Integer>) crewQueue).peekFirst();
-                if (targetCrewTime <= validTime) {
-                    lastBoardTime = ((LinkedList<Integer>) crewQueue).removeFirst();
-                    onBoardCnt++;
-                }
+            int shtTime = 540 + i * t;
+            int psgCnt = 0;
+            
+            while (timeList.size() > 0 && timeList.get(0) <= shtTime && psgCnt < m) {
+                lastArvTime = timeList.remove(0);
+                psgCnt++;
             }
-
-            if (i == n - 1) {
-                if (onBoardCnt == m) {
-                    conPassTime = lastBoardTime - 1;
-                }
-                if (onBoardCnt < m) {
-                    conPassTime = last;
-                }
-            }
+            
+            if (i == n - 1 && psgCnt == m) isLastShuttleFull = true;
         }
-
-        String answer = parseTime(conPassTime);
-        return answer;
-    }
-
-    private static String parseTime(int conPassTime) {
-        int hours = conPassTime / 60;
-        int minutes = conPassTime % 60;
-        return String.format("%02d:%02d", hours, minutes);
-    }
-
-    private static int parseInt(String s) {
-        return Integer.parseInt(s);
+        
+        int conArvTime = (isLastShuttleFull)? lastArvTime - 1 : 540 + (n - 1) * t;
+                
+        return String.format("%02d:%02d", conArvTime / 60, conArvTime % 60);
     }
 }
