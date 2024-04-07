0a07a762d544423e70a3922a6012d74572e1240f
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-04-07 23:55:00 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.08 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,64 +1,51 @@
 class Solution {
-    int n, m;
+    int grooveCnt = 0;
+
     public boolean solution(int[][] key, int[][] lock) {
-       m = key.length;
-        n = lock.length;
+        for (int[] row : lock) {
+            for (int num : row) if (num == 0) grooveCnt++;
+        }
 
-        int hom = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(lock[i][j] == 0) hom++;
+        boolean validKey = false;
+
+        for (int i = 0; i < 4; i++) {
+            if (i > 0) key = rotateKey(key);
+            if (validKey = isValidKey(key, lock)) break;
+        }
+
+        return validKey;
+    }
+
+    private int[][] rotateKey(int[][] key) {
+        int[][] rotatedKey = new int[key.length][key.length];
+
+        for (int i = 0; i < key.length; i++) {
+            for (int j = 0; j < key.length; j++) {
+                rotatedKey[j][key.length - 1 - i] = key[i][j];
             }
         }
 
-        for(int roll = 0; roll < 4; roll++) {    
-            for(int v = -m+1; v < n; v++) {     //수직 이동
-                for(int h = -m+1; h < n; h++) { //수평 이동   
-                    int cnt = 0;
-                    for(int i = 0; i < m; i++) {  
-                        if(i+v < 0 || i+v >= n) continue;
-                        for(int j = 0; j < m; j++) {
-                            if(j+h < 0 || j+h >= n) continue;
-                            if(lock[i+v][j+h] == 0) cnt++;
-                        }
+        return rotatedKey;
+    }
+
+    private boolean isValidKey(int[][] key, int[][] lock) {        
+        for (int i = -(key.length - 1); i < lock.length; i++) {
+            for (int j = -(key.length - 1); j < lock.length; j++) {
+                int matchedCnt = 0;
+
+                curLoop :
+                for (int x = 0; x < key.length; x++) {
+                    for (int y = 0; y < key.length; y++) {
+                        if (!(0 <= x + i && x + i < lock.length) || !(0 <= y + j && y + j < lock.length)) continue;
+                        if (key[x][y] == 1 && lock[x + i][y + j] == 1) break curLoop;
+                        if (key[x][y] == 1 && lock[x + i][y + j] == 0) matchedCnt++;
                     }
-
-                    if(cnt != hom) continue;
-                    boolean flag = true;
-                    for(int i = 0; flag && i < m; i++) {  
-                        if(i+v < 0 || i+v >= n) continue;
-                        for(int j = 0; flag && j < m; j++) {
-                            if(j+h < 0 || j+h >= n) continue;
-                            if(key[i][j] + lock[i+v][j+h] != 1) {
-                                flag = false;
-                                break;
-                            }
-                        }
-                    }                   
-                    if(flag) return true;
                 }
+
+                if (matchedCnt == grooveCnt) return true;
             }
-            key = spin(key);
         }
+
         return false;
-    }
-
-    public boolean check(int[][] lock, int[][] key) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(lock[i][j] + key[i][j] != 1) return false;
-            }
-        }
-        return true;
-    }
-
-    public int[][] spin(int[][] a) {
-        int[][] b = new int[n][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < m; j++) {
-                b[i][j] = a[m-1-j][i]; 
-            }
-        }
-        return b;
     }
 }
