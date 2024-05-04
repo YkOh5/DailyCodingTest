c977ee7bbdb94d55c722bf5ba4cbae4e99aaa941
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-05-04 23:28:41 +0900

    [level 3] Title: 표 편집, Time: 350.75 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -71,3 +71,85 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+
+// import java.util.*;
+
+// class Solution {    
+//     public String solution(int n, int k, String[] cmds) {
+//         Stack<Integer> stack = new Stack<>();
+//         for (String cmd: cmds) {
+//             StringTokenizer st = new StringTokenizer(cmd);
+//             String cmdType = st.nextToken();
+//             int d = 0;
+//             if (st.hasMoreTokens()) d = Integer.parseInt(st.nextToken());
+            
+//             switch (cmdType) {
+//                     case "U" -> k -= d;
+//                     case "D" -> k += d;
+//                     case "C" -> { 
+//                         stack.push(k);
+//                         n--;
+//                         if (k >= n) k--;
+//                     }
+//                     case "Z" -> {
+//                         if (stack.pop() <= k) k++;
+//                         n++;                        
+//                     }
+//             }            
+//         }
+                
+//         StringBuilder sb = new StringBuilder("O".repeat(n));
+//         while (!stack.isEmpty()) {
+//             sb.insert(stack.pop(), "X");
+//         }
+        
+//         return sb.toString();
+//     }
+// }
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public String solution(int n, int k, String[] cmds) {
+//         LinkedList<Integer> numList = new LinkedList<>();
+//         for (int i = 0; i < n; i++) numList.add(i);
+        
+//         int cursor = k;
+//         Stack<int[]> stack = new Stack<>();
+//         for (String cmd : cmds) {
+//             StringTokenizer st = new StringTokenizer(cmd);
+//             String cmdType = st.nextToken();
+//             int d = 0;
+//             if (st.hasMoreTokens()) d = Integer.parseInt(st.nextToken());
+                        
+//             switch (cmdType) {
+//                 case "U" -> cursor -= d;
+//                 case "D" -> cursor += d;
+//                 case "C" -> {
+//                     stack.push(new int[]{cursor, numList.remove(cursor)});
+//                     if (cursor > numList.size() - 1) cursor = numList.size() - 1;
+//                 }
+//                 case "Z" -> {
+//                     int[] temp = stack.pop();
+//                     numList.add(temp[0], temp[1]);
+//                     if (temp[0] <= cursor) cursor++;
+//                 }
+//             }
+//         }
+        
+//         boolean[] check = new boolean[n];
+//         for (int num : numList) check[num] = true;
+        
+//         StringBuilder sb = new StringBuilder();
+//         for (boolean b : check) {
+//             if (b) sb.append("O");
+//             else sb.append("X");
+//         }
+        
+//         return sb.toString();
+//     }
+// }
