b5cbc2b8f3e45b77f156b130e8623807c81d7bc0
YkOh5 <122243104+YkOh5@users.noreply.github.com>
2024-05-04 23:23:16 +0900

    [level 3] Title: 표 편집, Time: 849.69 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,31 +1,8 @@
 import java.util.*;
 
-class Solution {
-    private class Node {
-        int data;
-        Node prev;
-        Node next;
-        
-        public Node(int data) {
-            this.data = data;
-        }
-    }
-    
+class Solution {    
     public String solution(int n, int k, String[] cmds) {
-        Node[] nodeArr = new Node[n];
-        for (int i = 0; i < n; i++) {
-            nodeArr[i] = new Node(i);
-            if (i > 0) {
-                nodeArr[i - 1].next = nodeArr[i];
-                nodeArr[i].prev = nodeArr[i - 1];
-            }
-        }
-        
-        Node head = nodeArr[0];
-        Node tail = nodeArr[n - 1];
-        Node cursor = nodeArr[k];
-        
-        Stack<Node> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         for (String cmd: cmds) {
             StringTokenizer st = new StringTokenizer(cmd);
             String cmdType = st.nextToken();
@@ -33,39 +10,23 @@ class Solution {
             if (st.hasMoreTokens()) d = Integer.parseInt(st.nextToken());
             
             switch (cmdType) {
-                    case "U" -> { for (int i = 0; i < d; i++) cursor = cursor.prev; }
-                    case "D" -> { for (int i = 0; i < d; i++) cursor = cursor.next; }
+                    case "U" -> k -= d;
+                    case "D" -> k += d;
                     case "C" -> { 
-                        stack.push(cursor);
-                        if (cursor == head) {
-                            cursor = cursor.next;
-                            cursor.prev = null;
-                            head = cursor;
-                        } else if (cursor == tail) {
-                            cursor = cursor.prev;
-                            cursor.next = null;
-                            tail = cursor;
-                        } else {
-                            cursor.prev.next = cursor.next;
-                            cursor.next.prev = cursor.prev;
-                            cursor = cursor.next;
-                        }
+                        stack.push(k);
+                        n--;
+                        if (k >= n) k--;
                     }
                     case "Z" -> {
-                        Node node = stack.pop();
-                        if (node.prev == null) head = node;
-                        else node.prev.next = node;
-                        if (node.next == null) tail = node;
-                        else node.next.prev = node;
+                        if (stack.pop() <= k) k++;
+                        n++;                        
                     }
             }            
         }
                 
-        StringBuilder sb = new StringBuilder("X".repeat(n));
-        cursor = head;
-        while (cursor != null) {
-            sb.setCharAt(cursor.data, 'O');
-            cursor = cursor.next;
+        StringBuilder sb = new StringBuilder("O".repeat(n));
+        while (!stack.isEmpty()) {
+            sb.insert(stack.pop(), "X");
         }
         
         return sb.toString();
